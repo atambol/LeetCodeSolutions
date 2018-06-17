0b731bf0bbea331d7de53bed070612b7b8523eda
Anirudha Tambolkar <atambol@ncsu.edu>
2018-06-17 00:15:18 -0400

    improved the performance for #50

--- 50. Pow(x, n)/solution.go
+++ 50. Pow(x, n)/solution.go
@@ -1,29 +1,3 @@
-func getpowers(n int) []int {
-    poweroftwo := []int{1}
-    var result []int
-    prev := 1
-    for i:=2; i <= n ; i=i*2 {
-        poweroftwo = append(poweroftwo, i)
-        if n == i {
-            result = append(result, i)
-            return result
-        }
-        prev = i
-    }
-
-    result = append(result, prev)
-    n = n - prev
-    for n > 0 {
-
-        if poweroftwo[len(poweroftwo)-1] <= n {
-        result = append(result, poweroftwo[len(poweroftwo)-1])
-            n = n - poweroftwo[len(poweroftwo)-1]
-        }
-        poweroftwo = poweroftwo[:len(poweroftwo)-1]
-    }
-    return result
-}
-
 func myPow(x float64, n int) float64 {
     var isNegative bool
     if n == 0 {
@@ -37,18 +11,15 @@ func myPow(x float64, n int) float64 {
         isNegative = false
     }
     
-    powers := getpowers(n)
     var result float64 = 1
     val := x
-    pow := 1
 
-    for len(powers) > 0 {
-        if powers[len(powers)-1] == pow {
-            powers = powers[:len(powers)-1]
-            result = result*float64(val)
+    for n > 0 {
+        if n&1 > 0 {
+            result = result*val
         }
-        pow = pow*2
         val = val*val
+        n = n>>1
     }
     if isNegative {
         return 1/result
