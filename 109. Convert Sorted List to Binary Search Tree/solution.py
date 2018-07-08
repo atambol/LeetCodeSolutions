38f4e7d2cefa384dad362067e441a835fe4d5971
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-07-08 13:51:35 -0400

    109. Convert Sorted List to Binary Search Tree

---
+++ 109. Convert Sorted List to Binary Search Tree/solution.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        # Reduce the linked list to an array as we need random access in O(1) time while constructing BST
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+        
+        # Use the array and solve the problem with a known solution
+        return self.sortedArrayToBST(nums)
+        
+
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        
+        mid = int(len(nums)/2)
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        
+        return root
