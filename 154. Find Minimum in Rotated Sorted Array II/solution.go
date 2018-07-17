c50ba3de8e1f281f8553083bcc7e5e8a0d5ca348
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-07-17 16:47:46 -0400

    154. Find Minimum in Rotated Sorted Array II

---
+++ 154. Find Minimum in Rotated Sorted Array II/solution.go
@@ -0,0 +1,36 @@
+func findMin(nums []int) int {
+    // Assumes there will be atleast one element
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    
+    // Get the Pivot's index
+    pivot := getPivot(nums, 0, len(nums)-1)
+    fmt.Println(pivot)
+    return nums[pivot]
+}
+
+func getPivot(nums []int, i int, j int) int {
+    if i == j - 1 {
+        if nums[i] > nums[j] {
+            return j
+        } else {
+            return i
+        }
+    } else {
+        mid := (i + j)/2
+        if nums[i] <= nums[mid] && nums[mid] <= nums[j] {
+            leftPivot := getPivot(nums, i, mid)
+            rightPivot := getPivot(nums, mid, j)
+            if nums[leftPivot] < nums[rightPivot] {
+                return leftPivot
+            } else {
+                return rightPivot
+            }
+        } else if nums[i] > nums[mid] {
+            return getPivot(nums, i, mid)
+        } else {
+            return getPivot(nums, mid, j)
+        }
+    }
+}
