da61105f94801fc8873b43f17c3d5b0a175bf993
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-10-26 22:00:26 -0400

    346. Moving Average from Data Stream

---
+++ 346. Moving Average from Data Stream/solution.py
@@ -0,0 +1,29 @@
+class MovingAverage:
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.size = size
+        self.queue = []
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+
+        if len(self.queue) >= self.size:
+            self.queue = self.queue[1:]
+            self.queue.append(val)
+        else:
+            self.queue.append(val)
+
+            
+        return sum(self.queue)/len(self.queue)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
