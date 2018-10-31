fd975e5eacee370764e90bf0e7f64dd7c3364700
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-10-31 15:18:37 -0400

    Update solution.py

--- 543. Diameter of Binary Tree/solution.py
+++ 543. Diameter of Binary Tree/solution.py
@@ -26,16 +26,19 @@ class Solution(object):
         if not root.left and not root.right:
             return 0, 0
         else:
+            # Get left subtree's length and dia
             left_len = 0
             left_dia = 0
             if root.left:
                 left_dia, left_len = self.mydiameterOfBinaryTree(root.left)
-                
+            
+            # Get right subtree's length and dia
             right_len = 0
             right_dia = 0
             if root.right:
                 right_dia, right_len = self.mydiameterOfBinaryTree(root.right)
                 
+            # Calculate the root's dia based on left and right subtrees
             root_dia = 0
             if root.left and root.right:
                 root_dia = right_len + left_len + 2
@@ -44,6 +47,7 @@ class Solution(object):
             elif root.right:
                 root_dia = right_len + 1
 
+            # Return the length and dia accordingly
             root_len = max(right_len, left_len) + 1
             root_dia = max(root_dia, left_dia, right_dia)
             
