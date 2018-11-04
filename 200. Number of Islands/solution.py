e9c9db03760ded6ecb4efe65dfc66deefeaca7e1
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-11-04 12:07:31 -0500

    Update solution.py

--- 200. Number of Islands/solution.py
+++ 200. Number of Islands/solution.py
@@ -1,27 +1,22 @@
-class Solution:
+class Solution(object):
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        count = 0
-        if not grid:
-            return count
+        num = 0
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    self.dfs(grid, i, j)
-                    count += 1
-        return count
-        
+            for j in range(len(grid[i])):
+                num += self.dfs(grid, i, j)
+        return num
         
     def dfs(self, grid, i, j):
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-            return
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] in ['-1', '0']:
+            return 0
         else:
-            grid[i][j] = "-1"
-            self.dfs(grid, i, j+1)
+            grid[i][j] = '-1'
             self.dfs(grid, i+1, j)
-            self.dfs(grid, i, j-1)
             self.dfs(grid, i-1, j)
-            
+            self.dfs(grid, i, j+1)
+            self.dfs(grid, i, j-1)
+            return 1
