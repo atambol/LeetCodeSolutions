9ffb04dc1d5b227597d52617de00b051d5a51c3b
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-11-10 03:45:23 -0500

    Update solution.py

--- 5. Longest Palindromic Substring/solution.py
+++ 5. Longest Palindromic Substring/solution.py
@@ -4,44 +4,39 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        sol = ""
         l = len(s)
-        pali = []*l
-     
-        start = 0
-        end = 0
-        
+        # Choose a point in string to expand around and check for palindrome
+        # expand around indices
         for i in range(l):
-            pali.append([])
-            for j in range(l):
-                    pali[i].append(i==j)
-            
-        for i in range(1, l):
-            if s[i] == s[i-1]:
-                pali[i-1][i] = True
-                start = i-1
-                end = i
-            else:
-                pali[i-1][i] = False
-        
-        if l >= 3:
-            base = 0
-            diff = 2 
+            j = k = i
 
-            while diff < l:
-                i = base
-                j = base + diff
-                while j < l:
-                    if pali[i+1][j-1] and s[i] == s[j]:
-                        pali[i][j] = True
-                        start = i
-                        end = j
-                    else:
-                        pali[i][j] = False
-                    i += 1
-                    j += 1
-                diff += 1
+            while j >= 0 and k < l:
+                if s[j] == s[k]:
+                    j -= 1
+                    k += 1
+                else:
+                    break
+            k -= 1
+            j += 1
+            if abs(k - j) + 1 > len(sol):
+                sol = s[j:k+1]
 
-        # for i in range(l):
-        #     print(pali[i])
-        return s[start:end+1]
-        
+                
+        # expand between indices
+        for i in range(l):
+            j = i
+            k = i + 1
+            while j >=0 and k < l:
+                if s[j] == s[k]:
+                    j -= 1
+                    k += 1
+                else:
+                    break
+            k -= 1
+            j += 1        
+            if k - j + 1 > len(sol):
+                sol = s[j:k+1]
+
+                
+        return sol
