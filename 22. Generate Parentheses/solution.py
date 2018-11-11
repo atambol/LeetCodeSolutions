29df44106151f84ef00c212a9cde11a3337e241f
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-11-11 17:38:54 -0500

    Update solution.py

--- 22. Generate Parentheses/solution.py
+++ 22. Generate Parentheses/solution.py
@@ -4,17 +4,24 @@ class Solution:
         :type n: int
         :rtype: List[str]
         """
+        # Edge case
         if n == 0:
             return []
-        elif n == 1:
-            return ["()"]
         else:
+            # for i = 0
             sol = ["()"]
+            
+            # for i = 1 .. n-1
             for i in range(1, n):
                 tmp = set()
+                # for every combination found until now
                 for s in sol:
+                    # for every point between adjacent characters in the combination
                     for j in range(len(s)):
+                        # insert a paranthesis
                         tmp.add(s[:j] + "()" + s[j:])
+                        
+                # Take care of repetitions due to symmetrical strings
                 sol = list(tmp)
                 
             return sol
