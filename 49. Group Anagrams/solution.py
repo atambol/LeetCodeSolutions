934b459c4d4c43664e7c5be8a19ce331b05b3231
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-11-12 16:31:18 -0500

    Update solution.py

--- 49. Group Anagrams/solution.py
+++ 49. Group Anagrams/solution.py
@@ -1,16 +1,17 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        memory = defaultdict(lambda: [])
-        for s in strs:
-            memory["".join(sorted(s))].append(s)
+        sol = {}
+        
+        for s in strs:        
+            key = "".join(sorted(s))
+            if key in sol:
+                sol[key].append(s)
+            else:
+                sol[key] = [s]
             
-        res = []
-        for key in memory.keys():
-            res.append(memory[key])
-                
-        return res
+        return list(sol.values())
+            
