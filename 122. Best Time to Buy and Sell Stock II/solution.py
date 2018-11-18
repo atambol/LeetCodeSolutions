9949897e46864bae43c4b8053ad5738b1e0e2fee
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-11-17 20:36:37 -0500

    Update solution.py

--- 122. Best Time to Buy and Sell Stock II/solution.py
+++ 122. Best Time to Buy and Sell Stock II/solution.py
@@ -6,18 +6,23 @@ class Solution:
         """
         profit = 0
         if len(prices) > 1:
+            # edge case of 2 elements only
             if len(prices) == 2:
                 if prices[0] < prices[1]:
                     return prices[1] - prices[0]
 
+            # for number of elements greater than 2
             minprice = prices[0]
             for i in range(1, len(prices)):
+                # Keep looking for the next peak
                 if prices[i] >= prices[i-1]:
                     continue
                 else:
+                    # once the peak starts to fall, calculate the local profit and update the minprice
                     if minprice < prices[i-1]:
                         profit += prices[i-1] - minprice
                     minprice = prices[i]
+            # check for the last element if it was a peak
             if prices[-1] - minprice > 0:
                 profit += prices[-1] - minprice
                 
