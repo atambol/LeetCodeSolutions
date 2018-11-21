2701f1b938c8a698a03fb28abb556d5cf7f99147
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-11-21 11:34:40 -0500

    238. Product of Array Except Self

--- 238. Product of Array Except Self/solution.py
+++ 238. Product of Array Except Self/solution.py
@@ -4,16 +4,20 @@ class Solution:
         :type nums: List[int]
         :rtype: List[int]
         """
+        prev = 1
+        sol = []
         
+        # start from left, go right and calculate product of prev elements
+        for num in nums:
+            sol.append(prev)
+            prev *= num
+            
+        # start from right and go left calculating the product
+        prev = 1
+        for i in range(len(nums)-1, -1, -1):
+            sol[i] *= prev
+            prev *= nums[i]
+    
+        return sol
+            
         
-        lproduct = [0]*len(nums)
-        lproduct[0] = 1
-        for i in range(1, len(nums)):
-            lproduct[i] = lproduct[i-1] * nums[i-1]
-            
-        rproduct = 1
-        for i in range(len(nums)-2, -1, -1):
-            rproduct *= nums[i+1]
-            lproduct[i] = lproduct[i] * rproduct
-            
-        return lproduct
