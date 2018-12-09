299e7db6e031d5a973e6b25769ef3eb47b4695e0
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-12-08 20:09:47 -0500

    904. Fruit Into Baskets

--- 904. Fruit Into Baskets/solution.py
+++ 904. Fruit Into Baskets/solution.py
@@ -1,44 +1,46 @@
-class Solution:
-    def __init__(self):
-        self.q = []
-
+class Solution(object):
     def totalFruit(self, tree):
         """
         :type tree: List[int]
         :rtype: int
         """
-        maxSize = 0
-        curSize = 0
-        lastCount = 0
-        for i, v in enumerate(tree):
-            # print(self.q, curSize)
-            if v in self.q:
-                curSize += 1
-                
-                # Update the count of last element
-                if v == self.q[-1]:
-                    lastCount += 1
-                else:
-                    self.insert(v)
-                    lastCount = 1
-            else:
-                if curSize > maxSize:
-                    maxSize = curSize
-                if len(self.q) == 0:
-                    curSize = 1
-                elif len(self.q) == 1:
-                    curSize += 1
-                else:
-                    curSize = 1 + lastCount
-                self.insert(v)
-                lastCount = 1
-        # print(self.q, curSize)
-        return max(maxSize, curSize)
-
-    def insert(self, e):
-        if len(self.q) == 2:
-            self.q.pop(0)
-        self.q.append(e)
-
-    
+        # max count of fruits in basket
+        maxCount = 0 
         
+        # count of fruits of two types in basket
+        basketCount = 0
+        
+        # count of previously seen fruit occurance
+        prevCount = 0
+        
+        # previous fruit
+        prev = None
+        
+        # basket that holds two unique fruits
+        basket = set()
+        
+        for fruit in tree:
+            # if the fruit is already in basket then just update prevCount
+            if fruit in basket:
+                if prev == fruit:
+                    prevCount += 1
+                else:
+                    prevCount = 1
+                basketCount += 1
+            # the fruit is not in the basket, update the basket
+            else:
+                if len(basket) < 2:
+                    basketCount += 1
+                else:
+                    if basketCount > maxCount:
+                        maxCount = basketCount
+                    for b in basket:
+                        if b != prev:
+                            basket.remove(b)
+                            break
+                    basketCount = 1 + prevCount
+                basket.add(fruit)
+                prevCount = 1
+            prev = fruit
+            
+        return max(maxCount, basketCount)
