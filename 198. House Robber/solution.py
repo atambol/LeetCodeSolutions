8ef37cbb8f451850884db7927be268b8fcd34446
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-12-13 19:57:16 -0500

    198. House Robber

--- 198. House Robber/solution.py
+++ 198. House Robber/solution.py
@@ -1,18 +1,29 @@
-class Solution(object):
+class Solution:
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        # edge cases
         if not nums:
             return 0
-        if len(nums) == 1:
-            return nums[0]
-        sol = [0]*len(nums)
-        sol[0] = nums[0]
-        sol[1] = nums[1]
+        if len(nums) < 3:
+            return max(nums)
         
-        for i in range(2, len(nums)):
-            sol[i] = max(sol[i-3], sol[i-2]) + nums[i]
+        # loot will be calculated bottom up using dynamic programming
+        loot = [0]*len(nums)
+        for i in range(len(nums)):
+            if i - 2 >= 0:
+                loot1 = loot[i-2]
+            else:
+                loot1 = 0
             
-        return max(sol[-1], sol[-2])
+            if i - 3 >= 0:
+                loot2 = loot[i-3]
+            else:
+                loot2 = 0                
+                
+            loot[i] = max(nums[i] + loot1, nums[i] + loot2)
+            
+        return max(loot[-1], loot[-2])
+        
