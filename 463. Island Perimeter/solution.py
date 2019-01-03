28ab51f5b9c9c724b6ef9dcb82cdf5b7a7ce1f0f
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-02 20:36:02 -0500

    Update solution.py

--- 463. Island Perimeter/solution.py
+++ 463. Island Perimeter/solution.py
@@ -1,25 +1,37 @@
-class Solution(object):
+class Solution:
     def islandPerimeter(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        if not grid or not grid[0]:
-            return 0
-        
+        count = 0
         m = len(grid)
-        peri = 0
+        n = len(grid[0])
         for i in range(m):
-            n = len(grid[i])
             for j in range(n):
                 if grid[i][j] == 1:
-                    neighbours = [[i,j+1],[i,j-1],[i-1,j],[i+1,j]]
-                    for neighbour in neighbours:
-                        if neighbour[0] >= m or neighbour[0] < 0 or neighbour[1] < 0 or neighbour[1] >= n:
-                            peri += 1
-                        else:
-                            if grid[neighbour[0]][neighbour[1]] == 0:
-                                peri += 1
-
-        return peri
-                            
+                    if i-1 >= 0:
+                        if grid[i-1][j] == 0:
+                            count += 1
+                    else:
+                        count += 1
+                    
+                    if i+1 < m:
+                        if grid[i+1][j] == 0:
+                            count += 1
+                    else:
+                        count += 1
+                        
+                    if j-1 >= 0:
+                        if grid[i][j-1] == 0:
+                            count += 1
+                    else:
+                        count += 1
+                    
+                    if j+1 < n:
+                        if grid[i][j+1] == 0:
+                            count += 1
+                    else:
+                        count += 1
+                    
+        return count            
