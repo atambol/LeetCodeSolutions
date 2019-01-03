4156e634bb1307ab930c3744d1c2ee1b1597bd89
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-02 22:46:36 -0500

    Update solution.py

--- 240. Search a 2D Matrix II/solution.py
+++ 240. Search a 2D Matrix II/solution.py
@@ -5,19 +5,25 @@ class Solution:
         :type target: int
         :rtype: bool
         """
-        if not matrix:
+        # edge cases
+        m = len(matrix)
+        if not m:
+            return False
+        n = len(matrix[0])
+        if not n:
             return False
         
-        i = 0
-        j = len(matrix[i]) - 1
+        # start from bottom left
+        i = m-1
+        j = 0
         
-        while i < len(matrix) and j >= 0:
+        # check for target in O(m+n)
+        while i >= 0 and j < n:
             if matrix[i][j] == target:
                 return True
-            elif matrix[i][j] > target:
-                j -= 1
+            elif matrix[i][j] < target:
+                j += 1
             else:
-                i += 1
+                i -= 1
                 
         return False
-                
