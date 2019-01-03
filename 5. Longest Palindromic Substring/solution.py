259cc48c16ff25cfa5b6c705e79ef227bc44245d
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-02 19:00:14 -0500

    5. Longest Palindromic Substring

--- 5. Longest Palindromic Substring/solution.py
+++ 5. Longest Palindromic Substring/solution.py
@@ -1,42 +1,49 @@
-class Solution:
+class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        sol = ""
-        l = len(s)
-        # Choose a point in string to expand around and check for palindrome
-        # expand around indices
-        for i in range(l):
-            j = k = i
-
-            while j >= 0 and k < l:
-                if s[j] == s[k]:
-                    j -= 1
-                    k += 1
-                else:
-                    break
-            k -= 1
-            j += 1
-            if abs(k - j) + 1 > len(sol):
-                sol = s[j:k+1]
-
+        # edge case
+        if len(s) < 2:
+            return s
+        
+        n = len(s)
+        # expand around an index
+        maxStr = ""
+        for i in range(n):
+            k = i - 1
+            j = i + 1
+            
+            # keep expanding until fault
+            while k >= 0 and j < n and s[k] == s[j]:
+                j += 1
+                k -= 1
+            
+            # recover
+            k += 1
+            j -= 1
+            
+            # calculate the expansion
+            if j-k+1 > len(maxStr):
+                maxStr = s[k:j+1]
                 
         # expand between indices
-        for i in range(l):
-            j = i
-            k = i + 1
-            while j >=0 and k < l:
-                if s[j] == s[k]:
-                    j -= 1
-                    k += 1
-                else:
-                    break
-            k -= 1
-            j += 1        
-            if k - j + 1 > len(sol):
-                sol = s[j:k+1]
-
+        for i in range(n-1):
+            k = i
+            j = i + 1
+            
+            # keep expanding until fault
+            while k >= 0 and j < n and s[k] == s[j]:
+                j += 1
+                k -= 1
+            
+            # recover
+            k += 1
+            j -= 1
+            
+            # calculate the expansion
+            if j-k+1 > len(maxStr):
+                maxStr = s[k:j+1]
                 
-        return sol
+        return maxStr
