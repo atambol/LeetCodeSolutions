bcdf89c2acafa0ea87931e284a2e2921ce4a00c9
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-02 22:57:09 -0500

    Update solution.py

--- 739. Daily Temperatures/solution.py
+++ 739. Daily Temperatures/solution.py
@@ -5,18 +5,22 @@ class Solution:
         :rtype: List[int]
         """
         stack = []
+        index = {}
         sol = []
-        for i in range(len(T)-1, -1, -1):
-            while stack and T[stack[-1]] <= T[i]:
-                stack.pop()
-
-            if stack:
-                sol.append(stack[-1] - i)
-            else:
-                sol.append(0)
-                
-            stack.append(i)
-        sol.reverse()
-        return sol
-                
         
+        # use a stack to keep track of previous temperatures
+        # use a dictionary to keep track of the index of prev temperatures
+        for i in range(len(T)-1, -1, -1):
+            t = T[i]
+            while stack and stack[-1] <= t:
+                stack.pop()
+                
+            if not stack:
+                sol.append(0)
+            else:
+                sol.append(index[stack[-1]] - i)
+                
+            index[t] = i
+            stack.append(t)
+        
+        return sol[::-1]
