30bf97dc5cad1eeacc9749963d3965c47c24cac4
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-03 23:49:12 -0500

    Update solution.py

--- 222. Count Complete Tree Nodes/solution.py
+++ 222. Count Complete Tree Nodes/solution.py
@@ -14,17 +14,16 @@ class Solution:
         if not root:
             return 0
         
-        left = self.getDepth(root.left)
-        right = self.getDepth(root.right)
-        
-        # we need to look further right in this case
+        left = self.getHeight(root.left)
+        right = self.getHeight(root.right)
         if left == right:
             return 2**left + self.countNodes(root.right)
         else:
             return 2**right + self.countNodes(root.left)
-
-    def getDepth(self, root):
-        if not root:
-            return 0
-        else:
-            return 1 + self.getDepth(root.left)
+        
+    def getHeight(self, root):
+        count = 0
+        while root:
+            count += 1
+            root = root.left
+        return count
