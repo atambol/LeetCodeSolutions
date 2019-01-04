1bbdf22a6245b2f4d3148ea01b0b4835f8e864d7
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-04 01:00:18 -0500

    Update solution.py

--- 449. Serialize and Deserialize BST/solution.py
+++ 449. Serialize and Deserialize BST/solution.py
@@ -13,11 +13,13 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        node = root
-        stack = []
-        preorder = []
+        if not root:
+            return ""
         
-        # calculate the preorder traversal of the BST using iterative method
+        # preorder traversal of tree
+        preorder = []
+        stack = []
+        node = root
         while node or stack:
             if node:
                 preorder.append(str(node.val))
@@ -25,9 +27,8 @@ class Codec:
                 node = node.left
             else:
                 node = stack.pop()
-
-        return ",".join(preorder)
-
+                
+        return "/".join(preorder)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -35,40 +36,44 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        # edge case
         if not data:
             return None
-        else:
-            # create the preorder list, reverse it to optimise pop operations
-            preorder = [ int(x) for x in data.split(",")[::-1]]
+        
+        preorder = data.split("/")
+        
+        # reverse the order to make it easy to pop
+        preorder.reverse()
+        
+        # create root node
+        root = TreeNode(int(preorder.pop()))
+        stack = [root]
+        
+        # while there are nodes in traversal
+        while preorder:
+            # pop a node
+            node = TreeNode(int(preorder.pop()))
             
-            # read the list from right to left due to reversal
-            # root node is the last element right now
-            root = TreeNode(preorder.pop())
-            stack = [root]
-            # untils there are elements on the preorder list
-            while preorder:
-                # pop the last element
-                node = TreeNode(preorder.pop())
-                parent = stack[-1]
+            # pop a parent
+            parent = stack.pop()
+            
+            # if the node's val is less than parent's val, it goes to the left
+            if node.val < parent.val:
+                parent.left = node
+                stack.append(parent)
+            else:
+                # else it goes to the right
+                # the parent is then chosen by popping as many parents from the stack 
+                # the parents are popped until they have a value less than that of node
+                while stack and stack[-1].val < node.val:
+                    parent = stack.pop()
+                parent.right = node
                 
-                # compare it with the parent node
-                if parent.val < node.val:
-                    # node > parent, find the largest parent in the stack smaller than node
-                    while stack and stack[-1].val < node.val:
-                        parent = stack.pop()
-                    
-                    # attach node to the right
-                    parent.right = node
-                else:
-                    # attack node to the left
-                    parent.left = node
-                    
-                # push the node back to the stack
-                stack.append(node)
-                
-            return root
-
+            # push the node back to the stack
+            stack.append(node)
+            
+        return root
+            
+        
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
