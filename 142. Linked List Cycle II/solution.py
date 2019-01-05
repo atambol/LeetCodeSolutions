612f3df244fede799e84b27dc3a49385b14759e2
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-04 23:26:42 -0500

    Update solution.py

--- 142. Linked List Cycle II/solution.py
+++ 142. Linked List Cycle II/solution.py
@@ -10,31 +10,27 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # edge cases
-        if not head:
-            return None
-        
-        # fast pointer and slow pointer technique to find cycle
         fptr = head
         sptr = head
-        prev = None
+        
+        # find cycle
         cycle = False
-        while fptr and fptr.next and not cycle:
-            prev = sptr
+        while fptr and fptr.next:
             fptr = fptr.next.next
             sptr = sptr.next
-            if sptr == fptr:
+            
+            if fptr == sptr:
                 cycle = True
+                break
                 
         if not cycle:
             return None
-        else:
-            # the meeting point of two pointers in not the answer
-            # cycle can begin anywhere in the list
-            # to find that, increment both pointers with same pace now
-            sptr = head
-            while sptr != fptr:
-                fptr = fptr.next
-                sptr = sptr.next
-                
-            return sptr
+        
+        # find the beginning of cycle
+        fptr = head
+        while sptr != fptr:
+            fptr = fptr.next
+            sptr = sptr.next
+            
+        return sptr
+        
