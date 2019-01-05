34d4fe115dcf98987eb33e8abf89966b557470ff
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-04 20:28:10 -0500

    Update solution-heap.py

--- 23. Merge k Sorted Lists/solution-heap.py
+++ 23. Merge k Sorted Lists/solution-heap.py
@@ -1,33 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+class Solution:
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        '''
-        This solution combines all the list vals into a single list and uses a heapsort. 
-        It then construct new nodes for each of the vals.
-        '''
-        # Extract vals into heap
+        # assume there are m list with n nodes each on average
+        # create a min heap for each node's value, O(mn*log(mn))
         heap = []
-        for l in lists:
-            while l:
-                heapq.heappush(heap, l.val)
-                l = l.next
+        for head in lists:
+            while head:
+                heapq.heappush(heap, head.val)
+                head = head.next
         
-        # Construct a new linked list with the vals form heap
+        # pop elements from heap and add it to a linked list
+        # total elements = mn, heappop = log(mn), Complexity = O(mn*log(mn))
         head = ListNode(None)
         node = head
-        while heap :
-            val = heapq.heappop(heap)
-            node.next = ListNode(val)
+        while heap:
+            node.next = ListNode(heapq.heappop(heap))
             node = node.next
-            
-        return head.next
         
+        return head.next
