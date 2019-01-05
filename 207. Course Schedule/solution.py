50fb1f97156b59ef08d9389a83b8a8460bd7c512
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-05 13:54:28 -0500

    Update solution.py

--- 207. Course Schedule/solution.py
+++ 207. Course Schedule/solution.py
@@ -1,45 +1,50 @@
-class Solution(object):
+class Solution:
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        # create graph
+        # create a graph
         graph = {}
-        for p, q in prerequisites:
-            if p in graph:
-                graph[p].add(q)
-            else:
-                graph[p] = set([q])
+        for c in range(numCourses):
+            graph[c] = set()
             
+        for c, p in prerequisites:
+            graph[c].add(p)
+                
         # dfs visits every branch
         # unvisited => visited[p] = 0
         # being visited => visited[p] = 1   (to detect cycle)
         # visited => visited[p] = 2         (optimisation to not visit again)
-        def dfs(p):
-            sol = True
-            if p in graph:
-                # being visited, so if encountered again there is a cycle
-                visited[p] = 1
-                for q in graph[p]:
-                    if visited[q] == 1:
-                        sol = False
-                        break
-                    else:
-                        if not dfs(q):
-                            sol = False
-                            break
-                # this path need not be visited again
-                visited[p] = 2
-            return sol
+        def dfs(visited, course):
+            if visited[course] == 1:
+                # the course is being taken, cycle detected
+                return False
             
-        
-        # dfs over each course to check for a cycle
-        visited = [0]*numCourses
-        for course in graph:
-            # only visit unvisited nodes
-            if visited[course] == 0:
-                if not dfs(course):
+            if visited[course] == 2:
+                # the course has been taken
+                return True
+            
+            # mark the course as being taken
+            visited[course] = 1
+            
+            # take all its prereqs
+            for prereq in graph[course]:
+                canTake = dfs(visited, prereq)
+                if not canTake:
                     return False
+            
+            # mark the course taken
+            visited[course] = 2
+            return True
+        
+        # go over each course and get its prereqs
+        visited = [0]*numCourses
+        for c in graph:
+            if visited[c] == 0:
+                sol = dfs(visited, c)
+                if not sol:
+                    return False
+            
         return True
