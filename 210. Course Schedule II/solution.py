1ec5adf68ae7a1b26a16c3d7e427f26db6e8bbd0
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-05 14:07:57 -0500

    Update solution.py

--- 210. Course Schedule II/solution.py
+++ 210. Course Schedule II/solution.py
@@ -1,39 +1,67 @@
-from collections import defaultdict
-
-class Solution(object):
+class Solution:
     def findOrder(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: List[int]
         """
-        # Create a mapping from prereq to course and vice versa
-        prereqToCourse = defaultdict(lambda: set())
-        courseToPrereq = defaultdict(lambda: set())
-    
-        for course, prereq in prerequisites:
-            prereqToCourse[prereq].add(course)
-            courseToPrereq[course].add(prereq)
+        # create a graph for courses and prereqs
+        graph = {}
+        for c in range(numCourses):
+            graph[c] = set()
             
-        # Get a list of courses with prereqs taken - starting point
-        prereqsTaken = [course for course in range(numCourses) if not courseToPrereq[course]]
+        for c, p in prerequisites:
+            graph[c].add(p)
+            
+        # dfs visits every branch
+        # course not taken => visited[p] = 0
+        # course being taken => visited[p] = 1   (to detect cycle)
+        # course has been taken => visited[p] = 2   (optimisation to not take again)
+        def dfs(visited, c):
+            
+            # only courses with visited[c] = 0 will be visited
+            visited[c] = 1
+            
+            # final order of courses
+            order = []
+            
+            # take every prerequisite
+            for p in graph[c]:
+                
+                # if it is being taken, cycle is present
+                if visited[p] == 1:
+                    return []
+                
+                # if it has been taken, dont need to retake it
+                if visited[p] == 2:
+                    continue
+                
+                # get the prerequisite's course order
+                prereq_order = dfs(visited, p)
+                
+                # if it is empty, there is not solution
+                if not prereq_order:
+                    return prereq_order
+                
+                # add prereq's order to courses order
+                order.extend(prereq_order)
+            
+            # add the course to the order at the end
+            order.append(c)
+            
+            # mark the course as taken
+            visited[c] = 2
+            
+            return order
         
-        # Loop over and start taking courses 
-        coursesTaken = []
-        while prereqsTaken:
-            prereqRecentlyTaken = []
-            for prereq in prereqsTaken:
-                coursesTaken.append(prereq)
-                for course in prereqToCourse[prereq]:
-                    courseToPrereq[course].remove(prereq)
-                    if not courseToPrereq[course]:
-                        prereqRecentlyTaken.append(course)
-            prereqsTaken = prereqRecentlyTaken
-                    
-        # If all courses are taken then there is a possible solution
-        if len(coursesTaken) == numCourses:
-            return coursesTaken
-        else:
-            return []
-            
-            
+        # visit each course in graph
+        visited = [0]*numCourses
+        sol = []
+        for c in graph:
+            if visited[c] == 0:
+                course_order = dfs(visited, c)
+                if not course_order:
+                    return course_order
+                sol.extend(course_order)
+                
+        return sol
