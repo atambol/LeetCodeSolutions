5c80d0ac3dfb787046e74ef9c7bd185f2499fa47
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-05 23:37:35 -0500

    Update solution.py

--- 152. Maximum Product Subarray/solution.py
+++ 152. Maximum Product Subarray/solution.py
@@ -1,27 +1,15 @@
-class Solution(object):
+class Solution:
     def maxProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        # maintain two lists
-        # pos contains the most positive product of subarray
-        # neg contains the most negative product of subarray
-        if nums[0] >= 0:
-            pos = nums[0]
-            neg = 0
-        else:
-            neg = nums[0]
-            pos = 0
+        neg = [nums[0]]
+        pos = [nums[0]]
         
-        # prod is our solution
-        prod = nums[0]
-        
-        # iterate through each number
-        for i, num in enumerate(nums[1:]):
-            # get the most positive and negative product for this iteration
-            pos, neg = max(neg*num, pos*num, num), min(neg*num, pos*num, num)
+        for num in nums[1:]:
+            options = [num, num*neg[-1], num*pos[-1]]
+            neg.append(min(options))
+            pos.append(max(options))
             
-            # check if there is a more positive product
-            prod = max(pos, prod)
-        return prod
+        return max(pos+neg)
