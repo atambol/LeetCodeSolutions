e8752d953f506b6c66bead29ab6bc6734dce8d85
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-05 22:25:41 -0500

    Update solution.py

--- 139. Word Break/solution.py
+++ 139. Word Break/solution.py
@@ -1,34 +1,30 @@
-class Solution(object):
+class Solution:
     def wordBreak(self, s, wordDict):
         """
         :type s: str
         :type wordDict: List[str]
         :rtype: bool
         """
-        # create a set for O(1) get operation
-        words = set(wordDict)
+        if not s:
+            return True
         
-        # DP to keep track of indices that have been already explored
-        searched = [False]*len(s)
-        def search(i):
-            # if the i is more than the length of s, then search is successful
-            if i >= len(s):
-                return True
-            # if i is already visited, dont do it again
-            elif searched[i]:
-                return False
-            else:
-                # mark i visited
-                searched[i] = True
+        visited = [0]*len(s)
+        return self.myWordBreak(s, 0, wordDict, visited)
+    
+    def myWordBreak(self, s, start, wordDict, visited):
+        if start == len(s):
+            return True
+        
+        if visited[start]:
+            return False
+        
+        for i in range(start, len(s)):
+            if s[start:i+1] in wordDict:
+                canSegment = self.myWordBreak(s, i+1, wordDict, visited)
+                if canSegment:
+                    return True
                 
-                # try out each word that can be formed from this index onwards
-                for j in range(i, len(s)):
-                    # if the word is valid, perform search on the rest of the substring
-                    if s[i:j+1] in words:
-                        sol = search(j+1)
-                        if sol:
-                            return sol
-                        
-                # if nothing works out, search is unsuccessful
-                return False
-        return search(0)
+        visited[start] = 1
+        return False
+        
+        
