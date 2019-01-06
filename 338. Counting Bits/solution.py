0651f711138fc082b976363c355e53a11ca22278
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-05 23:46:33 -0500

    Update solution.py

--- 338. Counting Bits/solution.py
+++ 338. Counting Bits/solution.py
@@ -1,18 +1,15 @@
-class Solution(object):
+class Solution:
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
         """
-        dp = [0, 1, 1]
-        if num < 3:
-            return dp[:num+1]
-        power = 2
-        for i in range(3, num + 1):
-            if i == power*2:
+        powOfTwo = 1
+        dp = [0, 1]
+        for i in range(2, num+1):
+            if powOfTwo*2 == i:
                 dp.append(1)
-                power = i
+                powOfTwo *= 2
             else:
-                dp.append(dp[power] + dp[i-power])
-                
-        return dp
+                dp.append(1+dp[i-powOfTwo])
+        return dp[:num+1]
