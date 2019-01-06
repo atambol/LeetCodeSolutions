c7784731b3bd0e3a42be98d1845b7b99028e7305
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-06 00:39:45 -0500

    Update solution-dp.py

--- 62. Unique Paths/solution-dp.py
+++ 62. Unique Paths/solution-dp.py
@@ -5,28 +5,31 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        # edge case
+        # edge cases
         if not m or not n:
             return 0
         
-        # initialise the sol array 
-        # the top most and left most row starts with 1
-        # the rest of cells are calculated based on the cells to their left and top
-        sol = []
+        if m == 1 or n == 1:
+            return 1
+        
+        # create a dp matrix
+        dp = []
         for i in range(m):
-            s = []
+            dp.append([0]*n)
+        
+        # one way to reach the first cell
+        dp[0][0] = 1
+        
+        # find ways for other cells
+        for i in range(m):
             for j in range(n):
-                if not i or not j:
-                    s.append(1)
-                else:
-                    s.append(0)
-            sol.append(s)
-        
-        # use dynamic programming to calculate the unique path count
-        # in row major fashion, count the paths from left to right
-        for i in range(1, m):
-            for j in range(1, n):
-                sol[i][j] = sol[i][j-1] + sol[i-1][j]
-        
-        # solution is in the last cell
-        return sol[-1][-1]
+                up = 0
+                if i - 1 >= 0:
+                    up = dp[i-1][j]
+                left = 0
+                if j - 1 >= 0:
+                    left = dp[i][j-1]
+                    
+                dp[i][j] = left + up + dp[i][j]
+                
+        return dp[-1][-1]
