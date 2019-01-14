b2f5104a70339f50fa3f7918dec0a4ad990ab431
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-14 14:31:13 -0500

    Update solution.py

--- 207. Course Schedule/solution.py
+++ 207. Course Schedule/solution.py
@@ -1,50 +1,50 @@
 class Solution:
+    def __init__(self):
+        self.courses = None
+        self.graph = {}
+        self.unvisited = 0
+        self.visiting = 1
+        self.visited = 2
+        
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        # create a graph
-        graph = {}
+        # create the graph
         for c in range(numCourses):
-            graph[c] = set()
+            self.graph[c] = set()
             
         for c, p in prerequisites:
-            graph[c].add(p)
-                
-        # dfs visits every branch
-        # unvisited => visited[p] = 0
-        # being visited => visited[p] = 1   (to detect cycle)
-        # visited => visited[p] = 2         (optimisation to not visit again)
-        def dfs(visited, course):
-            if visited[course] == 1:
-                # the course is being taken, cycle detected
-                return False
-            
-            if visited[course] == 2:
-                # the course has been taken
+            self.graph[c].add(p)
+
+        # maintain visited status of courses
+        self.courses = [self.unvisited] * numCourses
+        
+        # check for each course if there is a loop
+        for c in range(numCourses):
+            if self.courses[c] == self.unvisited:
+                loop = self.dfs(c)
+                if loop:
+                    return False
+
+        return True
+    
+    def dfs(self, c):
+        if self.courses[c] == self.visiting:
+            return True
+        if self.courses[c] == self.visited:
+            return False
+        self.courses[c] = self.visiting
+        
+        for p in self.graph[c]:
+            loop = self.dfs(p)
+            if loop:
                 return True
             
-            # mark the course as being taken
-            visited[course] = 1
+        self.courses[c] = self.visited
+        return False
             
-            # take all its prereqs
-            for prereq in graph[course]:
-                canTake = dfs(visited, prereq)
-                if not canTake:
-                    return False
             
-            # mark the course taken
-            visited[course] = 2
-            return True
-        
-        # go over each course and get its prereqs
-        visited = [0]*numCourses
-        for c in graph:
-            if visited[c] == 0:
-                sol = dfs(visited, c)
-                if not sol:
-                    return False
             
-        return True
