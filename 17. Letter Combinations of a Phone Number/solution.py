4672b71124fcac01e3f3e2a4b77c72472ce42776
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-15 22:32:30 -0500

    Update solution.py

--- 17. Letter Combinations of a Phone Number/solution.py
+++ 17. Letter Combinations of a Phone Number/solution.py
@@ -1,30 +1,31 @@
-map = {
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"],
-}
-
-class Solution(object):        
+class Solution:
+    def __init__(self):
+        self.map = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        sol = []
-        solution = Solution()
-        if digits == "":
+        if not digits:
             return []
-        if digits[1:] == "":
-            return map[digits[0]]
-        else:
-            strings = solution.letterCombinations(digits[1:])
-            for alpha in map[digits[0]]:
-                # print alpha
-                for string in strings:
-                    sol.append(alpha + string)
-            return sol
+        if len(digits) == 1:
+            return self.map[digits[0]]
+        
+        combination = self.map[digits[0]]
+        child_combination = self.letterCombinations(digits[1:])
+        sol = []
+        for i in combination:
+            for j in child_combination:
+                sol.append(i+j)
+                
+        return sol
