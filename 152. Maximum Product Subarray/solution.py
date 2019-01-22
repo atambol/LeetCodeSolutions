5a0071c28879afbad2194bd5e61e2274a63a60d9
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-22 00:47:26 -0500

    Update solution.py

--- 152. Maximum Product Subarray/solution.py
+++ 152. Maximum Product Subarray/solution.py
@@ -4,12 +4,13 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        neg = [nums[0]]
-        pos = [nums[0]]
-        
+        neg = nums[0]
+        pos = nums[0]
+        prod = nums[0]
         for num in nums[1:]:
-            options = [num, num*neg[-1], num*pos[-1]]
-            neg.append(min(options))
-            pos.append(max(options))
+            candidates = [neg*num, pos*num, num]
+            neg = min(candidates)
+            pos = max(candidates)
+            prod = max(pos, prod)
             
-        return max(pos+neg)
+        return prod
