cca49040019406fcc9d3df7d2c9252e8cb383d33
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-21 22:41:11 -0500

    Update solution.py

--- 22. Generate Parentheses/solution.py
+++ 22. Generate Parentheses/solution.py
@@ -4,19 +4,22 @@ class Solution:
         :type n: int
         :rtype: List[str]
         """
+        sol = set()
+        
         # edge case
         if not n:
             return []
         
-        sol = set(["()"])
-        if n == 1:
-            return list(sol)
+        # base case
+        sol.add("()")
         
-        for i in range(n-1):
+        # generate 
+        for i in range(1, n):
             newsol = set()
+            # for every combination previously seen
             for s in sol:
-                for j in range(len(s)):
-                    newsol.add(s[:j] + "()" + s[j:])
+                for i in range(len(s)):
+                    newsol.add(s[:i] + "()" + s[i:])
                     
             sol = newsol
             
