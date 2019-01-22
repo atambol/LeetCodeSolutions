8ab814cf52eb40d22eb391b2cbc263fb7866fd02
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-21 21:40:17 -0500

    Update solution.py

--- 547. Friend Circles/solution.py
+++ 547. Friend Circles/solution.py
@@ -1,29 +1,24 @@
-
-class Solution(object):
+class Solution:
     def findCircleNum(self, M):
         """
         :type M: List[List[int]]
         :rtype: int
         """
-        n = len(M)
-        visited = [False]*n
         count = 0
+        m = len(M)
+        visited = [False]*m
         
-        # dfs over a user
-        def dfs(friends, user, visited):
-            if visited[user]:
-                return
-            visited[user] = True
-            for friend, friendship in enumerate(friends[user]):
-                if friendship:
-                    dfs(friends, friend, visited)
-                    
-        # visit every user and mark it's circle
-        for user in range(n):
-            if not visited[user]:
+        # count the friend circles
+        for i in range(m):
+            if not visited[i]:
+                self.dfs(i, visited, M)
                 count += 1
-                dfs(M, user, visited)
-                
-        return count
             
+        return count
+    
+    def dfs(self, i, visited, M):
+        visited[i] = True
         
+        for j in range(len(M[i])):
+            if M[i][j] and not visited[j]:
+                self.dfs(j, visited, M)
