37a15c99dc06420d54b5bc0fc9ef62b2ac1c2b60
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-22 00:05:55 -0500

    Update solution.py

--- 139. Word Break/solution.py
+++ 139. Word Break/solution.py
@@ -1,30 +1,48 @@
 class Solution:
+    def __init__(self):
+        self.visited = []
+        self.maxSize = 0
+        self.words = set()
+        
     def wordBreak(self, s, wordDict):
         """
         :type s: str
         :type wordDict: List[str]
         :rtype: bool
         """
-        if not s:
+        # visited list
+        self.visited = [False]*len(s)
+        
+        # calculate the maxsize of a word
+        for w in wordDict:
+            self.maxSize = max(self.maxSize, len(w))
+        
+        # O(n) lookup for a word
+        self.words.update(wordDict)
+        
+        return self.myWordBreak(s, 0)
+        
+    def myWordBreak(self, s, i):
+        # base condition
+        if i == len(s):
             return True
         
-        visited = [0]*len(s)
-        return self.myWordBreak(s, 0, wordDict, visited)
-    
-    def myWordBreak(self, s, start, wordDict, visited):
-        if start == len(s):
-            return True
-        
-        if visited[start]:
+        # visited array - memoization
+        if self.visited[i]:
             return False
         
-        for i in range(start, len(s)):
-            if s[start:i+1] in wordDict:
-                canSegment = self.myWordBreak(s, i+1, wordDict, visited)
-                if canSegment:
+        # mark i visited
+        self.visited[i] = True
+        
+        # calculate the limit of the word
+        l = min(i + self.maxSize + 1, len(s)+1)
+        
+        # check if word in set
+        for j in range(i+1, l):
+            if s[i:j] in self.words:
+                isBreak = self.myWordBreak(s, j)
+                if isBreak:
                     return True
                 
-        visited[start] = 1
         return False
-        
         
