0762652309bfcbf9fce333629a558d4a98b6614a
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-22 01:25:17 -0500

    Create solution.py

---
+++ 518. Coin Change 2/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0]*(amount+1)
+        
+        dp[0] = 1
+        
+        for c in coins:
+            for i in range(1, amount+1):
+                if i >= c:
+                    dp[i] += dp[i-c]
+
+        return dp[-1]
