e88d4c5dfe763d8e4b8dc236ac8ce1f0c2b69dca
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-21 19:09:38 -0500

    Update solution.py

--- 124. Binary Tree Maximum Path Sum/solution.py
+++ 124. Binary Tree Maximum Path Sum/solution.py
@@ -1,55 +1,43 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
+class Solution:
     def maxPathSum(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        MaxPath, lMaxSum = self.myMaxPathSum(root)
-        return lMaxSum
-        
-    def myMaxPathSum(self, root):
+        maxSum, maxPath = self.mymaxPathSum(root)
+        return maxSum
+    
+    def mymaxPathSum(self, root):
         """
         :type root: TreeNode
-        :rtype: int, int
+        :rtype: int
         """
-        if root.left and root.right:
-            lMaxPath, lMaxSum = self.myMaxPathSum(root.left)
-            rMaxPath, rMaxSum = self.myMaxPathSum(root.right)
-            possibleSums = [
-                            root.val + lMaxPath + rMaxPath, # self, right and left
-                            root.val + lMaxPath, # self and left
-                            root.val + rMaxPath, # self and right
-                            lMaxSum, # left only
-                            rMaxSum, # right only
-                            root.val # self only
-                           ]
-            possiblePaths = [rMaxPath + root.val, lMaxPath + root.val, root.val]
-            return max(possiblePaths), max(possibleSums)
-        elif root.left and not root.right:
-            lMaxPath, lMaxSum = self.myMaxPathSum(root.left)
-            possibleSums = [
-                            root.val + lMaxPath, # self and left
-                            lMaxSum, # left only
-                            root.val # self only
-                           ]
-            possiblePaths = [lMaxPath + root.val, root.val]
-            return max(possiblePaths), max(possibleSums)
-        elif not root.left and root.right:
-            rMaxPath, rMaxSum = self.myMaxPathSum(root.right)
-            possibleSums = [
-                            root.val + rMaxPath, # self and right
-                            rMaxSum, # right only
-                            root.val # self only
-                           ]
-            possiblePaths = [rMaxPath + root.val, root.val]
-            return max(possiblePaths), max(possibleSums)
-        else:
+        # not child
+        if not root.left and not root.right:
             return root.val, root.val
-                
+        # only right child
+        elif not root.left and root.right:
+            rSum, rPath = self.mymaxPathSum(root.right)
+            maxPath = max(rPath+root.val, root.val)
+            maxSum = max(rSum, maxPath)
+            return maxSum, maxPath
+        # only left child
+        elif not root.right and root.left:
+            lSum, lPath = self.mymaxPathSum(root.left)
+            maxPath = max(lPath+root.val, root.val)
+            maxSum = max(lSum, maxPath)
+            return maxSum, maxPath
+        # both child
+        else:
+            rSum, rPath = self.mymaxPathSum(root.right)
+            lSum, lPath = self.mymaxPathSum(root.left)
+            maxPath = max(lPath+root.val, rPath+root.val, root.val)
+            maxSum = max(lSum, rSum, lPath+root.val+rPath, maxPath)
+            return maxSum, maxPath
