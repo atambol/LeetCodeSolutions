a31cbbad22a9d0bcb007316b6d7a02a0c2c89929
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-21 22:29:14 -0500

    Update solution.py

--- 3. Longest Substring Without Repeating Characters/solution.py
+++ 3. Longest Substring Without Repeating Characters/solution.py
@@ -4,25 +4,13 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        # edge cases
-        n = len(s)
-        if n < 2:
-            return n
-        
-        # run two pointer i and j
-        i = 0
-        maxLen = 0
-        
-        # maintain a map of character to indices
-        map = {}
-        
-        for j in range(n):
-            if s[j] in map:
-                i = max(map[s[j]] + 1, i) ## this step is important
-            map[s[j]] = j
-            maxLen = max(maxLen, j-i+1)
+        count = 0
+        j = 0
+        seen = {}
+        for i in range(len(s)):
+            if s[i] in seen:
+                count = max(count, i - j)
+                j = max(j, seen[s[i]] + 1)
+            seen[s[i]] = i
             
-        return maxLen
-                
-                
-                
+        return max(count, len(s) - j)
