9fbe7ed5b74e26e1799d5151d787aedd28f6dc0c
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-21 22:56:07 -0500

    Update solution.py

--- 463. Island Perimeter/solution.py
+++ 463. Island Perimeter/solution.py
@@ -4,34 +4,27 @@ class Solution:
         :type grid: List[List[int]]
         :rtype: int
         """
-        count = 0
+        # edge case
+        peri = 0
+        if not grid or not grid[0]:
+            return peri
+        
+        # traverse the grid
         m = len(grid)
         n = len(grid[0])
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    if i-1 >= 0:
-                        if grid[i-1][j] == 0:
-                            count += 1
-                    else:
-                        count += 1
+                    if i - 1 < 0 or grid[i-1][j] == 0:
+                        peri += 1
                     
-                    if i+1 < m:
-                        if grid[i+1][j] == 0:
-                            count += 1
-                    else:
-                        count += 1
+                    if j - 1 < 0 or grid[i][j-1] == 0:
+                        peri += 1
                         
-                    if j-1 >= 0:
-                        if grid[i][j-1] == 0:
-                            count += 1
-                    else:
-                        count += 1
-                    
-                    if j+1 < n:
-                        if grid[i][j+1] == 0:
-                            count += 1
-                    else:
-                        count += 1
-                    
-        return count            
+                    if j + 1 >= n or grid[i][j+1] == 0:
+                        peri += 1
+                        
+                    if i + 1 >= m or grid[i+1][j] == 0:
+                        peri += 1
+                        
+        return peri
