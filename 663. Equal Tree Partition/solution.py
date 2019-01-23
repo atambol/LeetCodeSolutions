5720f38fc968ecd578598e9b09cf6fe211575c1c
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-22 22:19:22 -0500

    Update solution.py

--- 663. Equal Tree Partition/solution.py
+++ 663. Equal Tree Partition/solution.py
@@ -1,41 +1,66 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
+class Solution:
     def checkEqualTree(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        subtreeSumList = []
-        queue = []
-
-        # Calculate the subtree sum with root node
-        def subtreeSum(node):
-            if not node:
-                return 0
-            
-            if not node.left and not node.right:
-                subtreeSumList.append(node.val)
-                return node.val
-            
-            mySubtreeSum = subtreeSum(node.left) + subtreeSum(node.right) + node.val
-            subtreeSumList.append(mySubtreeSum)
-            
-            return mySubtreeSum
+        if not root:
+            return False
         
-        # Get the total tree's sum
-        treeSum = subtreeSum(root)
+        stack = []
+        node = root
+        total = 0
         
-        # Compare subtree total with tree total
-        for subtreeSum in subtreeSumList[:-1]:
-            if treeSum == subtreeSum * 2:
-                return True
+        while node or stack:
+            if node:
+                total += node.val
+                stack.append(node.right)
+                node = node.left
+            else:
+                node = stack.pop()
+        
+        if total%2 == 1:
+            return False
+        
+        half = total/2
+        _, status = self.dfs(root, half)
+        return status
+    
+    def dfs(self, root, target):
+        if root.left and root.right:
+            left, status = self.dfs(root.left, target)
+            if status:
+                return None, status
+            right, status = self.dfs(root.right, target)
+            if status:
+                return None, status
+            if left == target or right == target:
+                return None, True
+            else:
+                return left + right + root.val, False
+        elif not root.left and root.right:
+            right, status = self.dfs(root.right, target)
+            if status:
+                return None, status
+            if right == target:
+                return None, True  
+            else:
+                return root.val + right, False
+        elif root.left and not root.right:
+            left, status = self.dfs(root.left, target)
+            if status:
+                return None, status
+            if left == target:
+                return None, True 
+            else:
+                return root.val + left, False
+        else:
+            return root.val, False
             
-        return False
-        
-        
