1c81c734d8c7209bbd96db79b200f884d65fd331
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-23 00:06:20 -0500

    Update solution.py

--- 54. Spiral Matrix/solution.py
+++ 54. Spiral Matrix/solution.py
@@ -1,52 +1,39 @@
 class Solution:
-    def __init__(self):
-        # direction transition
-        self.nextDir = {
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        dir = {
             (0, 1): (1, 0),
             (1, 0): (0, -1),
             (0, -1): (-1, 0),
             (-1, 0): (0, 1)
         }
         
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
         sol = []
-        
-        # edge case
-        if not matrix or not matrix[0]:
+        m = len(matrix)
+        if not m:
+            return sol
+        n = len(matrix[0])
+        if not n:
             return sol
         
-        # starting points and direction
-        i = 0
-        j = 0
-        dir = (0, 1)
-        
-        # constraints
-        seen = set()
-        m = len(matrix)
-        n = len(matrix[0])
+        x, y = 0, 1
+        i, j = 0, 0
+        visited = set()
         count = m*n
-        
-        # spiral in
-        while len(seen) != count:
-            # change direction if constrained
-            if i < 0 or i >= m or j < 0 or j >= n or (i, j) in seen:
-                i = i-dir[0]
-                j = j-dir[1]
-                
-                dir = self.nextDir[dir]
-                i = i+dir[0]
-                j = j+dir[1]    
-            
-            # record the spiral and mark the cells seen
-            sol.append(matrix[i][j])
-            seen.add((i, j))
-            
-            # next cell
-            i = i+dir[0]
-            j = j+dir[1] 
+        while count:
+            while i >= 0 and j >= 0 and i < m and j < n and (i, j) not in visited:
+                visited.add((i, j))
+                sol.append(matrix[i][j])
+                count -= 1
+                i += x
+                j += y
+            i -= x
+            j -= y
+            x, y = dir[(x, y)]
+            i += x
+            j += y
             
         return sol
