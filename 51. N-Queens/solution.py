aef35e2dfe67bcc05b0e91def859b7b308da8799
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-28 22:24:46 -0500

    Update solution.py

--- 51. N-Queens/solution.py
+++ 51. N-Queens/solution.py
@@ -1,40 +1,48 @@
 class Solution:
     def __init__(self):
         self.sol = []
-
+        
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
         """
-        config = set()
-        self.backtrack(n, 0, config)
+        config = []
+        self.backtrack(n, config)
         return self.sol
-        
-    def backtrack(self, n, i, config):
-        if i == n:
-            self.saveSol(n, config)
-            
-        for j in range(n):
-            if self.isSafe(i, j, config):
-                config.add((i, j))
-                self.backtrack(n, i+1, config)
-                config.remove((i, j))
-                    
-    def isSafe(self, i, j, config):
-        for x, y in config:
-            if i == x or j == y or (abs(i-x) == abs(j-y)):
-                return False
-            
-        return True
     
-    def saveSol(self, n, config):
+    def backtrack(self, n, config):
+        if len(config) == n:
+            self.saveConfig(n, config)
+        else:
+            for i in range(n):
+                isSafe = self.checkSafe(config, i)
+                if isSafe:
+                    config.append(i)
+                    self.backtrack(n, config)
+                    config.pop()
+                    
+    def checkSafe(self, config, y):
+        x = len(config)
+        for i in range(len(config)):
+            if i == x or y == config[i]:
+                return False
+            if abs(i-x) == abs(config[i]-y):
+                return False
+        
+        return True    
+    
+    def saveConfig(self, n, config):
         sol = []
         for i in range(n):
             sol.append(["."]*n)
             
-        for x, y in config:
-            sol[x][y] = "Q"
+        for i in range(len(config)):
+            sol[i][config[i]] = "Q"
             
-        strSol = ["".join(s) for s in sol]
-        self.sol.append(strSol)
+        sol2 = []
+        for i in range(n):
+            sol2.append("".join(sol[i]))
+            
+        self.sol.append(sol2)
+        
