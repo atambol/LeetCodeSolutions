29a020c8c30199100da0d5cd3ced139e109f0e34
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-01-28 20:47:53 -0500

    Update solution.py

--- 139. Word Break/solution.py
+++ 139. Word Break/solution.py
@@ -1,8 +1,31 @@
 class Solution:
     def __init__(self):
-        self.visited = []
-        self.maxSize = 0
-        self.words = set()
+        self.visited = None
+        self.maxsize = 0
+        self.words = None
+        
+    def dfs(self, s, i):
+        # all words found
+        if i == len(s):
+            return True
+        
+        # visited list
+        if self.visited[i]:
+            return False
+        
+        # mark visited
+        self.visited[i] = True
+        
+        for j in range(i+1, i + self.maxsize + 1):
+            if j > len(s):
+                return False
+            
+            if s[i:j] in self.words:
+                sol = self.dfs(s, j)
+                if sol:
+                    return True
+
+        return False
         
     def wordBreak(self, s, wordDict):
         """
@@ -10,39 +33,21 @@ class Solution:
         :type wordDict: List[str]
         :rtype: bool
         """
-        # visited list
-        self.visited = [False]*len(s)
+        n = len(s)
         
-        # calculate the maxsize of a word
-        for w in wordDict:
-            self.maxSize = max(self.maxSize, len(w))
-        
-        # O(n) lookup for a word
-        self.words.update(wordDict)
-        
-        return self.myWordBreak(s, 0)
-        
-    def myWordBreak(self, s, i):
-        # base condition
-        if i == len(s):
-            return True
-        
-        # visited array - memoization
-        if self.visited[i]:
+        # edge case
+        if not n:
             return False
         
-        # mark i visited
-        self.visited[i] = True
+        # get the max size of the dictionary
+        for w in wordDict:
+            self.maxsize = max(self.maxsize, len(w))
+            
+        # create a dictionary from the list of words
+        self.words = set(wordDict)
         
-        # calculate the limit of the word
-        l = min(i + self.maxSize + 1, len(s)+1)
+        # visited set to improve performance
+        self.visited = [False]*n
         
-        # check if word in set
-        for j in range(i+1, l):
-            if s[i:j] in self.words:
-                isBreak = self.myWordBreak(s, j)
-                if isBreak:
-                    return True
-                
-        return False
-        
+        # perform dfs
+        return self.dfs(s, 0)
