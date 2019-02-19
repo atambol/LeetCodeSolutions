3fc4f567718beeae2da71f1c37d40c132fe90d33
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-02-18 22:45:28 -0500

    Update solution.py

--- 34. Find First and Last Position of Element in Sorted Array/solution.py
+++ 34. Find First and Last Position of Element in Sorted Array/solution.py
@@ -4,15 +4,21 @@ class Solution:
         
     def search(self, nums, target, low, high):
         sol = [-1, -1]
+        # base condition
         if low > high:
             return sol
+        
+        # only one element between the indices
         elif low == high:
             if nums[low] == target:
                 sol[0] = low
                 sol[1] = low
             return sol
         else:
+            # calculate mid
             mid = (low+high)//2
+            
+            # if mid is the target, look both sides
             if nums[mid] == target:
                 sol1 = self.search(nums, target, low, mid-1)
                 sol2 = self.search(nums, target, mid+1, high)
@@ -22,8 +28,12 @@ class Solution:
                 if sol2[1] != -1:
                     sol[1] = sol2[1]
                 return sol
+            
+            # mid is less than target, look right
             elif nums[mid] < target:
                 return self.search(nums, target, mid+1, high)
+            
+            # mid is greater than target, look left
             else:
                 return self.search(nums, target, low, mid-1)
                     
