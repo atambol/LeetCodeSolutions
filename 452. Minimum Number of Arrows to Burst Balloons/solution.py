c92fb6478f5a214ec79efc82760e5825e689bee0
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-02-27 13:18:27 -0500

    Update solution.py

--- 452. Minimum Number of Arrows to Burst Balloons/solution.py
+++ 452. Minimum Number of Arrows to Burst Balloons/solution.py
@@ -1,23 +1,17 @@
 class Solution:
-    def findMinArrowShots(self, points: 'List[List[int]]') -> 'int':
-        # edge case
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
         if not points:
             return 0
         
-        # this problem is similar to meeting rooms 2
-        arrows = 0
-        n = len(points)
-        points.sort(key=lambda x: (x[0], x[1]))
+        points.sort(key=lambda x:(x[0], x[1]))
+        count = 0
         
-        # greedy approach
-        i = 0
-        while i < n:
-            y = points[i][1]
-            j = i + 1
-            while j < n and points[j][0] <= y:
-                y = min(y, points[j][1]) # all points seen so far should have a common edge
-                j += 1
-            arrows += 1
-            i = j
-            
-        return arrows
+        end = points[0][1]
+        for x, y in points[1:]:
+            if x > end:
+                count += 1
+                end = y
+            else:
+                end = min(end, y)
+                
+        return count+1
