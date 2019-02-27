dc7ff3b4ac604a14353875cfd97c4e8765fa9f8c
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-02-27 16:10:22 -0500

    Update solution.py

--- 239. Sliding Window Maximum/solution.py
+++ 239. Sliding Window Maximum/solution.py
@@ -1,40 +1,30 @@
 class Solution:
-    def maxSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         # edge case
-        if not nums or not k or k == 1:
-            return nums
+        if not nums:
+            return []
         
-        # use a dequeue
+        # create the first window
         deq = collections.deque()
-        deq.append(0)
-        sol = []
-        
-        # run the logic for the first window
-        for i in range(1, k):
-            while deq and nums[deq[-1]] < nums[i]:
-                deq.pop()
-            deq.append(i)
-        sol.append(nums[deq[0]])
-        
-        # loop over each window and recalculate max num
-        for i in range(k, len(nums)):
-            # remove deq elements that are outside the window
-            while deq and i - deq[0] >= k:
-                deq.popleft()
-            
-            # remove deq elements that are smaller than nums[i]
-            while deq and nums[deq[-1]] < nums[i]:
+        for i in range(k):
+            while deq and nums[i] > nums[deq[-1]]:
                 deq.pop()
                 
-            # store the newest index
             deq.append(i)
-                    
-            # append the largest element
-            sol.append(nums[deq[0]])
+            
+        # maintain a solution for each window
+        sol = []
+        sol.append(nums[deq[0]])
         
+        # process the remaining windows
+        for i in range(k,len(nums)):
+            while deq and i - deq[0] >= k:
+                deq.popleft()
+                
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+                
+            deq.append(i)
+            sol.append(nums[deq[0]])
+            
         return sol
