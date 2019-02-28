ca440d6a043dc6a727b271097a9cd8659a20fc89
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-02-28 02:21:21 -0500

    Update solution.py

--- 449. Serialize and Deserialize BST/solution.py
+++ 449. Serialize and Deserialize BST/solution.py
@@ -16,7 +16,6 @@ class Codec:
         if not root:
             return ""
         
-        # preorder traversal of tree
         preorder = []
         stack = []
         node = root
@@ -28,7 +27,7 @@ class Codec:
             else:
                 node = stack.pop()
                 
-        return "/".join(preorder)
+        return ".".join(preorder)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -39,41 +38,25 @@ class Codec:
         if not data:
             return None
         
-        preorder = data.split("/")
-        
-        # reverse the order to make it easy to pop
+        preorder = data.split(".")
         preorder.reverse()
-        
-        # create root node
         root = TreeNode(int(preorder.pop()))
         stack = [root]
-        
-        # while there are nodes in traversal
         while preorder:
-            # pop a node
             node = TreeNode(int(preorder.pop()))
-            
-            # pop a parent
             parent = stack.pop()
-            
-            # if the node's val is less than parent's val, it goes to the left
             if node.val < parent.val:
                 parent.left = node
                 stack.append(parent)
             else:
-                # else it goes to the right
-                # the parent is then chosen by popping as many parents from the stack 
-                # the parents are popped until they have a value less than that of node
                 while stack and stack[-1].val < node.val:
                     parent = stack.pop()
+                    
                 parent.right = node
-                
-            # push the node back to the stack
             stack.append(node)
-            
         return root
             
-        
+
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
