f117380a30126c56e9f61dfdc0355d48d4951df6
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-02-27 23:07:27 -0500

    Update solution.py

--- 404. Sum of Left Leaves/solution.py
+++ 404. Sum of Left Leaves/solution.py
@@ -6,15 +6,11 @@
 #         self.right = None
 
 class Solution:
-    def sumOfLeftLeaves(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
         if not root:
             return 0
         
-        if root.left and not root.left.right and not root.left.left:
+        if root.left and not root.left.left and not root.left.right:
             return root.left.val + self.sumOfLeftLeaves(root.right)
         else:
             return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
