2f51f2c6f581d7af14d3925b271ac78b3ad1b61c
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-02-28 00:18:32 -0500

    Update solution.py

--- 545. Boundary of Binary Tree/solution.py
+++ 545. Boundary of Binary Tree/solution.py
@@ -6,57 +6,55 @@
 #         self.right = None
 
 class Solution:
-    def boundaryOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        boundary = []
+    def boundaryOfBinaryTree(self, root: TreeNode) -> List[int]:
+        sol = []
         if not root:
-            return boundary
-        
-        # add root
-        boundary.append(root.val)
-        
-        # add left side
-        node = root.left
-        while node:
-            boundary.append(node.val)
-            if node.left:
-                node = node.left
-            else:
-                node = node.right
-                
-        if len(boundary) > 1:
-            boundary.pop()
-        
-        # add the bottom
+            return sol
+
+        sol.append(root.val)
+
+        # left
+        left = []
+        if root.left:
+            node = root.left
+            while node:
+                left.append(node.val)
+                if node.left:
+                    node = node.left
+                else:
+                    node = node.right
+
+            left.pop()
+        sol.extend(left)
+
+        # bottom
         stack = [root.right]
-        preorder = []
         node = root.left
         while node or stack:
             if node:
                 if not node.left and not node.right:
-                    boundary.append(node.val)
+                    sol.append(node.val)
                     node = None
                 else:
                     stack.append(node.right)
                     node = node.left
             else:
                 node = stack.pop()
-        
-        # add the right side
-        sol = []
-        node = root.right
-        while node:
-            sol.append(node.val)
-            if node.right:
-                node = node.right
-            else:
-                node = node.left
-        if sol:
-            sol.pop()
-        sol.reverse()
-        boundary.extend(sol)
-        
-        return boundary
+
+        # right
+        right = []
+        if root.right:
+            node = root.right
+            while node:
+                right.append(node.val)
+                if node.right:
+                    node = node.right
+                else:
+                    node = node.left
+
+            right.pop()
+            right.reverse()
+
+        sol.extend(right)
+
+        return sol
