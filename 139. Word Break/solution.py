3f231a3a35469361340bd7adeb290d61a2561c30
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-01 09:16:21 -0500

    Update solution.py

--- 139. Word Break/solution.py
+++ 139. Word Break/solution.py
@@ -1,53 +1,34 @@
-class Solution:
-    def __init__(self):
-        self.visited = None
-        self.maxsize = 0
-        self.words = None
-        
-    def dfs(self, s, i):
-        # all words found
-        if i == len(s):
-            return True
-        
-        # visited list
-        if self.visited[i]:
-            return False
-        
-        # mark visited
-        self.visited[i] = True
-        
-        for j in range(i+1, i + self.maxsize + 1):
-            if j > len(s):
-                return False
-            
-            if s[i:j] in self.words:
-                sol = self.dfs(s, j)
-                if sol:
-                    return True
-
-        return False
-        
+class Solution(object):
     def wordBreak(self, s, wordDict):
         """
         :type s: str
         :type wordDict: List[str]
         :rtype: bool
         """
-        n = len(s)
+        words = set(wordDict)
+        visited = [False]*len(s)
+        return self.search(s, words, visited, 0)
+    
+    # basic dfs
+    def search(self, s, words, visited, i):
+        # check if the string is exhausted
+        if i == len(s):
+            return True
         
-        # edge case
-        if not n:
+        # check if visited
+        if visited[i]:
             return False
         
-        # get the max size of the dictionary
-        for w in wordDict:
-            self.maxsize = max(self.maxsize, len(w))
+        # mark visited
+        visited[i] = True
+        j = i
+        
+        # dfs over the s
+        while j < len(s):
+            if s[i:j+1] in words:
+                res = self.search(s, words, visited, j+1)
+                if res:
+                    return res
+            j += 1
             
-        # create a dictionary from the list of words
-        self.words = set(wordDict)
-        
-        # visited set to improve performance
-        self.visited = [False]*n
-        
-        # perform dfs
-        return self.dfs(s, 0)
+        return False
