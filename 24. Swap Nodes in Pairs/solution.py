77acfc9502c53b12368e98b6ced0ab3750f4f845
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-01 19:56:46 -0500

    Update solution.py

--- 24. Swap Nodes in Pairs/solution.py
+++ 24. Swap Nodes in Pairs/solution.py
@@ -5,29 +5,22 @@
 #         self.next = None
 
 class Solution:
-    def swapPairs(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        # Handle edge cases
+    def swapPairs(self, head: ListNode) -> ListNode:
         if not head or not head.next:
             return head
-        
-        # General flow
-        ptr = ListNode(None)
-        ptr.next = head
-        head = ptr
-        ptr = ptr.next 
-        prev = head
-        
-        while ptr and ptr.next:
-            tmp = ptr.next
-            ptr.next = ptr.next.next
-            tmp.next = prev.next
-            prev.next = tmp
-            
-            prev = ptr
-            ptr = ptr.next
 
-        return head.next
+        # weave in and out to extract nodes
+        ptr = head
+        dummy = ListNode(None)
+        prev = dummy
+        while ptr and ptr.next:
+            tmp = ptr.next.next
+            prev.next = ptr.next
+            prev.next.next = ptr
+            ptr.next = tmp
+            prev = ptr
+            ptr = tmp
+        
+        return dummy.next
+                
+        
