57f7dc62dbeaf8fa12bd4f6b016f01b465c23a00
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-02 15:06:50 -0500

    Update solution.py

--- 46. Permutations/solution.py
+++ 46. Permutations/solution.py
@@ -1,21 +1,33 @@
 class Solution:
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        if len(nums) <= 1:
-            return [nums]
-        
-        if len(nums) == 2:
-            return [
-                nums,
-                nums[::-1]
-            ]
-            
+    def permute(self, nums: List[int]) -> List[List[int]]:
         sol = []
-        for i in range(len(nums)):
-            for p in self.permute(nums[:i] +  nums[i+1:]):
-                sol.append([nums[i]] + p)
-                
+        # edge case
+        if not nums:
+            return sol
+        
+        numset = set(nums)
+        
+        # backtrack
+        return self.backtrack(numset)
+        
+    # O(n!) for backtracking over numset of size n
+    def backtrack(self, numset):    
+        sol = []
+        
+        # base case
+        if len(numset) == 1:
+            num = numset.pop()
+            sol.append([num])
+            numset.add(num)
+        else:
+            # loop over n numbers
+            nums = list(numset)
+            for num in nums:
+                numset.remove(num)
+                sub = self.backtrack(numset)
+                for s in sub:
+                    s.append(num)
+                    sol.append(s)
+                numset.add(num)
         return sol
+            
