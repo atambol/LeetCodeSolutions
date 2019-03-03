4649d2b1f64ed5cb892eafca293044e957de6dac
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-03 06:02:26 -0500

    Update solution.py

--- 70. Climbing Stairs/solution.py
+++ 70. Climbing Stairs/solution.py
@@ -1,22 +1,11 @@
 class Solution:
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        prev1 = 1
-        prev2 = 2
+    def climbStairs(self, n: int) -> int:
         if n == 1:
-            return prev1
-        
+            return 1
         if n == 2:
-            return prev2
-            
+            return 2
+        dp = [0, 1, 2]
         for i in range(3, n+1):
-            tmp = prev1 + prev2
-            prev1 = prev2
-            prev2 = tmp
+            dp.append(dp[i-1] + dp[i-2])
             
-        return prev2
-            
-        
+        return dp[-1]
