27636623f9b96780d69762f6e93ace243d7e4fa1
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-04 14:09:22 -0500

    Update solution.py

--- 92. Reverse Linked List II/solution.py
+++ 92. Reverse Linked List II/solution.py
@@ -1,41 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def reverseBetween(self, head, m, n):
-        """
-        :type head: ListNode
-        :type m: int
-        :type n: int
-        :rtype: ListNode
-        """
-        # attach a dummy head
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        # edge case
+        if m == n:
+            return head
+    
+        # insert dummy head
         node = ListNode(None)
         node.next = head
         head = node
         node = head.next
         prev = head
         
-        # find the mth node
-        l = 0
-        while m > 1:
+        # reach the mth position
+        while m-1:
             prev = node
             node = node.next
             m -= 1
             n -= 1
-            
-        # break the list here
-        tail1 = prev
-        head2 = node
         
-        # reverse the next n - m nodes
+        tail1 = prev
+        tail1.next = None
+        head2 = node
         prev = None
-        while n >= 1:
+        
+        # reach the nth position
+        while n:
+            prev = node
+            node = node.next
+            n -= 1
+            
+        tail2 = prev
+        tail2.next = None
+        head3 = node
+        
+        # reverse between head2 and tail2
+        node = head2
+        prev = None
+        while node:
             tmp = node.next
             node.next = prev
             prev = node
             node = tmp
-            n -= 1
             
-        # join the lists
-        tail1.next = prev
-        head2.next = node
+        # attach the lists
+        tail1.next = tail2
+        head2.next = head3
         
         return head.next
