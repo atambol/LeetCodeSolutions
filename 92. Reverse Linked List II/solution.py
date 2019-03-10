a23c6f417b86082f35a429997222fd8bd3c1d484
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-10 17:06:45 -0400

    Update solution.py

--- 92. Reverse Linked List II/solution.py
+++ 92. Reverse Linked List II/solution.py
@@ -6,50 +6,33 @@
 
 class Solution:
     def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-        # edge case
-        if m == n:
-            return head
-    
-        # insert dummy head
+        # attach dummy head
         node = ListNode(None)
         node.next = head
         head = node
-        node = head.next
-        prev = head
         
-        # reach the mth position
-        while m-1:
+        # reach mth node
+        prev = head
+        node = head.next
+        while m != 1:
             prev = node
             node = node.next
             m -= 1
             n -= 1
         
+        # reverse the nodes
         tail1 = prev
-        tail1.next = None
-        head2 = node
+        tail2 = node
         prev = None
-        
-        # reach the nth position
         while n:
-            prev = node
-            node = node.next
-            n -= 1
-            
-        tail2 = prev
-        tail2.next = None
-        head3 = node
-        
-        # reverse between head2 and tail2
-        node = head2
-        prev = None
-        while node:
             tmp = node.next
             node.next = prev
             prev = node
             node = tmp
+            n -= 1
             
-        # attach the lists
-        tail1.next = tail2
-        head2.next = head3
-        
+        # attach all parts together
+        tail1.next = prev
+        tail2.next = node
         return head.next
+        
