ee226468f8133ed50ed43488982deace3fc0e103
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-10 19:12:04 -0400

    Update solution.py

--- 22. Generate Parentheses/solution.py
+++ 22. Generate Parentheses/solution.py
@@ -1,22 +1,14 @@
 class Solution:
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        sol = set()
+    def generateParenthesis(self, n: int) -> List[str]:
+        sol = []
+        if n == 0:
+            return sol
+        sol.append("()")
+        if n == 1:
+            return sol
         
-        # edge case
-        if not n:
-            return []
-        
-        # base case
-        sol.add("()")
-        
-        # generate 
         for i in range(1, n):
             newsol = set()
-            # for every combination previously seen
             for s in sol:
                 for i in range(len(s)):
                     newsol.add(s[:i] + "()" + s[i:])
