e870e747c84cd9a3f3fef9d4eaf162232b597815
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-10 21:39:04 -0400

    Update solution.py

--- 207. Course Schedule/solution.py
+++ 207. Course Schedule/solution.py
@@ -1,50 +1,43 @@
 class Solution:
     def __init__(self):
-        self.courses = None
         self.graph = {}
-        self.unvisited = 0
-        self.visiting = 1
         self.visited = 2
+        self.visiting = 1
+        self.unvisited = 0
         
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        # create the graph
-        for c in range(numCourses):
-            self.graph[c] = set()
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # create graph
+        for i in range(numCourses):
+            self.graph[i] = set()
             
         for c, p in prerequisites:
             self.graph[c].add(p)
-
-        # maintain visited status of courses
-        self.courses = [self.unvisited] * numCourses
-        
-        # check for each course if there is a loop
-        for c in range(numCourses):
-            if self.courses[c] == self.unvisited:
-                loop = self.dfs(c)
-                if loop:
+            
+        # dfs
+        status = [self.unvisited]*numCourses
+        for i in range(numCourses):
+            if status[i] == self.unvisited:
+                cycle = self.dfs(i, status)
+                if cycle:
                     return False
-
+                
         return True
     
-    def dfs(self, c):
-        if self.courses[c] == self.visiting:
+    def dfs(self, course, status):
+        if status[course] == self.visiting:
             return True
-        if self.courses[c] == self.visited:
-            return False
-        self.courses[c] = self.visiting
         
-        for p in self.graph[c]:
-            loop = self.dfs(p)
-            if loop:
+        if status[course] == self.visited:
+            return False
+        
+        status[course] = self.visiting
+        
+        for prerequisite in self.graph[course]:
+            cycle = self.dfs(prerequisite, status)
+            if cycle:
                 return True
             
-        self.courses[c] = self.visited
+        status[course] = self.visited
         return False
-            
-            
-            
+                
+                
