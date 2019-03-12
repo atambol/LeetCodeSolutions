b8566b67c067e9565f78ecdabcfaea48c7a6cb51
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-11 22:59:43 -0400

    Update solution.py

--- 39. Combination Sum/solution.py
+++ 39. Combination Sum/solution.py
@@ -1,23 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         # edge case
-        if not candidates and not target:
-            return []
+        if not candidates:
+            return sol
         
-        # backtrack over each target
-        candidates.sort()
+        # sort the candidates
+        candidates.sort()        
         return self.backtrack(candidates, target, 0)
+
         
-    def backtrack(self, candidates, target, index):
+    def backtrack(self, candidates, target, i):
         sol = []
-        for i, c in enumerate(candidates[index:]):
-            if target == c:
-                sol.append([c])
-            elif target > c:
-                subsol = self.backtrack(candidates, target-c, i+index)
-                for s in subsol:
-                    sol.append([c]+s)
-            else:
+        for j in range(i, len(candidates)):
+            if target - candidates[j] == 0:
+                sol.append([candidates[j]])
                 break
+            elif target - candidates[j] < 0:
+                break
+            else:
+                sub = self.backtrack(candidates, target-candidates[j], j)
+                for s in sub:
+                    sol.append([candidates[j]] + s)
                 
         return sol
