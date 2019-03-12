9c3472e3c39a14e881f7a61841feb37d9c713bc8
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-11 21:43:02 -0400

    Update solution.py

--- 79. Word Search/solution.py
+++ 79. Word Search/solution.py
@@ -1,36 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # edge cases
         if not board or not word:
             return False
         
-        # perform dfs on every cell with starting letter of word
-        n = len(board)
-        m = len(board[0])
-        for i in range(n):
-            for j in range(m):
-                visited = set()
-                res = self.dfs(board, i, j, word, 0, visited, m, n)
-                if res:
-                    return True
+        visited = set()
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    if self.dfs(board, visited, i, j, word, 0):
+                        return True
+        
         return False
+        
+    def dfs(self, board, visited, i, j, word, k):
+        if k == len(word):
+            return True
+        
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or (i, j) in visited or word[k] != board[i][j]:
+            return False
+        
+        visited.add((i, j))
+        res = self.dfs(board, visited, i-1, j, word, k+1) or \
+              self.dfs(board, visited, i, j-1, word, k+1) or \
+              self.dfs(board, visited, i+1, j, word, k+1) or \
+              self.dfs(board, visited, i, j+1, word, k+1) 
+        
+        visited.remove((i, j))
+        return res
     
-    def dfs(self, board, i, j, word, k, visited, m, n):
-        # check if within range
-        if 0 <= i < n and 0 <= j < m and (i, j) not in visited and board[i][j] == word[k]:
-            # check if end of word
-            k += 1
-            if k == len(word):
-                return True
         
-            # keep doing dfs
-            visited.add((i, j))
-            res = False
-            res = res or self.dfs(board, i+1, j, word, k, visited, m, n)
-            res = res or self.dfs(board, i-1, j, word, k, visited, m, n)
-            res = res or self.dfs(board, i, j+1, word, k, visited, m, n)
-            res = res or self.dfs(board, i, j-1, word, k, visited, m, n)
-            visited.remove((i, j))
-            return res
-        
-        return False
