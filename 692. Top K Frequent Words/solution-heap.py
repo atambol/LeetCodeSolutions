a15d03e2a814e7649bf5166a462fcaae446af064
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-11 22:07:10 -0400

    Update solution-heap.py

--- 692. Top K Frequent Words/solution-heap.py
+++ 692. Top K Frequent Words/solution-heap.py
@@ -1,31 +1,21 @@
 class Solution:
-    def topKFrequent(self, words, k):
-        """
-        :type words: List[str]
-        :type k: int
-        :rtype: List[str]
-        """
-        # count the word frequency
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # get freq
         freq = {}
         for word in words:
             try:
-                freq[word] = freq[word] + 1
-            except:
+                freq[word] += 1
+            except KeyError:
                 freq[word] = 1
                 
-        # add the (-freq, word) tuple to a list
-        # reverse the frew using negative sign
-        # python heaps are min heaps by default
-        # this would push the most freq element to the top
-        # also, heap orders the element using both count and word
-        # so there is no need to use a key to sort words lexicographically in case of a tie
-        wordfreq = [(-f, w) for w,f in freq.items()]
-        
-        # heapify the list
-        heapq.heapify(wordfreq)
-        
-        # select the smallest k elements
-        topk = heapq.nsmallest(k, wordfreq)
-        
-        # return only the words from tuple list
-        return [i[1] for i in topk]
+        # create a heap
+        heap = []
+        for w, f in freq.items():
+            heapq.heappush(heap, (-f, w))
+                
+        # extract words
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+            
+        return res
