81f4d1c37dd0c180cd236d9d1a87be833ad7d3c3
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-12 23:34:13 -0400

    Update solution.py

--- 516. Longest Palindromic Subsequence/solution.py
+++ 516. Longest Palindromic Subsequence/solution.py
@@ -1,29 +1,25 @@
-class Solution:
+class Solution(object):
     def longestPalindromeSubseq(self, s):
         """
         :type s: str
         :rtype: int
         """
-        # edge cases
-        n = len(s)
+        # edge case
         if not s:
             return 0
         
-        # dynamic programming
+        # prepare for dp
         dp = []
+        n = len(s)
         for i in range(n):
             dp.append([0]*n)
             
+        # check for palindromic subsequences
         for i in range(n-1, -1, -1):
-            # palindrome around itself
             dp[i][i] = 1
-            
-            # look at all the substring from i + 1 -> n
-            for j in range(i+1, n):
-                # if same chars then look at the first cell anti diagonally
+            for j in range(i + 1, n):
                 if s[i] == s[j]:
-                    dp[i][j] = dp[i+1][j-1] + 2
-                # if not, it is the maximum of what is seen so far to the left and below
+                    dp[i][j] = 2 + dp[i+1][j-1]
                 else:
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1])
                     
