8c81de5fdacfee3bc1060af68d7256d80fa84909
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-12 22:33:37 -0400

    Update solution.py

--- 154. Find Minimum in Rotated Sorted Array II/solution.py
+++ 154. Find Minimum in Rotated Sorted Array II/solution.py
@@ -1,16 +1,21 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return self.find(nums, 0, len(nums)-1)
-    
-    def find(self, nums, low, high):
-        if high-low <= 1:
-            return min(nums)
+        return self.myFindMin(nums, 0, len(nums)-1)
         
-        mid = (high+low)//2
-        if nums[low] == nums[high]:
-            return min(self.find(nums, low, mid), self.find(nums, mid+1, high))
-                       
-        if mid < high:
-            return min(nums[mid], self.find(nums, low, mid-1))
+    def myFindMin(self, nums, low, high):
+        if high - low == 1:
+            return min(nums[low], nums[high])
+        elif high - low == 0:
+            return nums[low]
+        
+        mid = (low+high)//2
+        
+        # if mid element is not giving clear indications, O(n)
+        if nums[mid] == nums[high] or nums[mid] == nums[low]:
+            return min(self.myFindMin(nums, low, mid-1), self.myFindMin(nums, mid, high))
+        # look on one side only O(logn)
+        if nums[mid] < nums[high]:
+            return min(self.myFindMin(nums, low, mid-1), nums[mid])
         else:
-            return min(nums[low], self.find(nums, mid+1, high))
+            return min(self.myFindMin(nums, mid+1, high), nums[low])
+        
