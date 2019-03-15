ac442f3dd57dd0941cec7d73978201bdf6e3ff44
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-14 22:15:12 -0400

    Update solution.py

--- 200. Number of Islands/solution.py
+++ 200. Number of Islands/solution.py
@@ -4,19 +4,27 @@ class Solution:
         :type grid: List[List[str]]
         :rtype: int
         """
+        # edge case
+        if not grid or not grid[0]:
+            return 0
+        
+        # count by dfs
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == '1':
-                    count += 1
+        n = len(grid)
+        m = len(grid[0])
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
                     self.dfs(grid, i, j)
+                    count += 1
         return count
-    
+                    
     def dfs(self, grid, i, j):
-        if i >= 0 and j >= 0 and i < len(grid) and j < len(grid[i]) and grid[i][j] == '1':
-            grid[i][j] = '-1'
-            self.dfs(grid, i-1, j)
-            self.dfs(grid, i, j-1)
-            self.dfs(grid, i, j+1)
-            self.dfs(grid, i+1, j)
-            
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
+            return
+        
+        grid[i][j] = "-1"
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j-1)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i+1, j)
