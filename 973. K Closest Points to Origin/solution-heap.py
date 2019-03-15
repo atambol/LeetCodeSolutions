53dd29d94b50b4bc7753fe188f79174c714e66a6
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-14 21:22:20 -0400

    Update solution-heap.py

--- 973. K Closest Points to Origin/solution-heap.py
+++ 973. K Closest Points to Origin/solution-heap.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def kClosest(self, points, K):
-        """
-        :type points: List[List[int]]
-        :type K: int
-        :rtype: List[List[int]]
-        """
-        # heap to maintain K closest points
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         heap = []
         for x, y in points:
-            dist=x*x + y*y
-            if len(heap) < K:
-                heapq.heappush(heap, (-dist, (x, y)))
-            elif len(heap) == K and abs(heap[0][0]) > dist:
-                heapq.heappushpop(heap, (-dist, (x, y)))
+            d = x*x+y*y
+            if len(heap) == K:
+                heapq.heappushpop(heap, (-d, (x, y)))
+            else:
+                heapq.heappush(heap, (-d, (x, y)))
                 
-        return [h[1] for h in heap]
+        return [p for d, p in heap]
