780aa498172e6c97342fc17d30b8e35af6b9cab6
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-14 22:39:39 -0400

    Update solution.py

--- 146. LRU Cache/solution.py
+++ 146. LRU Cache/solution.py
@@ -1,93 +1,63 @@
 class Node:
-    def __init__(self, k, v):
-        self.val = v
-        self.key = k
-        self.prev = None
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
         self.next = None
-
+        self.prev = None
+        
 class LRUCache:
-
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+    def __init__(self, capacity: int):
+        self.cap = capacity
         self.head = Node(None, None)
         self.tail = Node(None, None)
         self.head.prev = self.tail
         self.tail.next = self.head
         self.map = {}
-        self.cap = capacity
-        
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key not in self.map:
-            return -1
-        node = self.map[key]
-        
-        # take the node out
-        node.next.prev = node.prev
-        node.prev.next = node.next
-        
-        # push it to the top
-        node.next = self.head
-        node.prev = self.head.prev
-        self.head.prev = node
-        node.prev.next = node
-        
-        return node.val
-        
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
-        # if the node exists in the list
+    def get(self, key: int) -> int:
+        head = self.head
+        tail = self.tail
+        
+        if key in self.map:
+            node = self.map[key]
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            node.prev = head.prev
+            head.prev = node
+            node.prev.next = node
+            node.next = head
+            return node.val
+        else:
+            return -1
+        
+    def put(self, key: int, value: int) -> None:
+        head = self.head
+        tail = self.tail
+
+        node = None    
         if key in self.map:
             node = self.map[key]
             node.val = value
-            
-            # take the node out
-            node.next.prev = node.prev
             node.prev.next = node.next
-
-            # push it to the top
-            node.next = self.head
-            node.prev = self.head.prev
-            self.head.prev = node
-            node.prev.next = node
+            node.next.prev = node.prev
         
-        # a new node should be created
-        else:
+        if not node:
             if not self.cap:
-                # take the least recently used node out of list
-                node = self.tail.next
-                self.tail.next = node.next
-                node.next.prev = self.tail
-                
-                # remove the node from map
+                node = tail.next
                 self.map.pop(node.key)
-                
+                node.prev.next = node.next
+                node.next.prev = node.prev
             else:
                 self.cap -= 1
-            
-            # create a new node
             node = Node(key, value)
-            
-            # push it to the top
-            node.next = self.head
-            node.prev = self.head.prev
-            self.head.prev = node
-            node.prev.next = node
-                      
-            # add it to the map
             self.map[key] = node
-            
+        
+        node.prev = head.prev
+        head.prev = node
+        node.prev.next = node
+        node.next = head
 
+        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
