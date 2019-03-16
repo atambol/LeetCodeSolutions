48b394a0f37a2cfd2074876f5dcf52944e3a3089
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-16 17:25:49 -0400

    Update solution.py

--- 139. Word Break/solution.py
+++ 139. Word Break/solution.py
@@ -1,34 +1,30 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        """
-        words = set(wordDict)
-        visited = [False]*len(s)
-        return self.search(s, words, visited, 0)
-    
-    # basic dfs
-    def search(self, s, words, visited, i):
-        # check if the string is exhausted
-        if i == len(s):
-            return True
-        
-        # check if visited
-        if visited[i]:
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # edge case
+        if not s or not wordDict:
             return False
         
-        # mark visited
-        visited[i] = True
-        j = i
+        # create word set
+        words = set(wordDict)
         
-        # dfs over the s
-        while j < len(s):
-            if s[i:j+1] in words:
-                res = self.search(s, words, visited, j+1)
-                if res:
-                    return res
-            j += 1
-            
+        # basic dfs
+        visited = [False]*len(s)
+        return self.dfs(words, visited, s, 0)
+    
+    def dfs(self, words, visited, string, start):
+        if visited[start]:
+            return False
+        
+        visited[start] = True
+        
+        for end in range(start, len(string)):
+            if string[start:end+1] in words:
+                if end == len(string) - 1:
+                    return True
+                else:
+                    res = self.dfs(words, visited, string, end+1)
+                    if res:
+                        return True
+                    
         return False
+            
