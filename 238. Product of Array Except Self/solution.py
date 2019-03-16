bda5734b23bd4a067150611a655abff73aa7ab0a
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-15 22:33:04 -0400

    Update solution.py

--- 238. Product of Array Except Self/solution.py
+++ 238. Product of Array Except Self/solution.py
@@ -1,34 +1,14 @@
 class Solution:
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        # edge cases
-        if not nums: 
-            return nums
-        
-        # calculate product from left
-        prev = nums[0]
-        left = [1]
-        
-        for num in nums[1:]:
-            left.append(prev)
-            prev *= num
-        
-        # calculate product from right
-        prev = nums[-1]
-        right = [1]
-        
-        for num in nums[-2::-1]:
-            right.append(prev)
-            prev *= num
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [0]*len(nums)
+        prod = 1
+        for i in range(len(nums)):
+            left[i] = prod
+            prod *= nums[i]
             
-        right.reverse()
-        
-        # calculate final sol
-        sol = []
-        for l, r in zip(left, right):
-            sol.append(l*r)
-
-        return sol
+        prod = 1
+        for i in range(len(nums)-1, -1, -1):
+            left[i] = left[i]*prod
+            prod *= nums[i]
+            
+        return left
