dcb5a0acb3514ee4727e8cc6bfd5d74a165b3944
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-19 13:00:18 -0400

    Update solution.py

--- 387. First Unique Character in a String/solution.py
+++ 387. First Unique Character in a String/solution.py
@@ -1,30 +1,14 @@
-class Solution(object):
-    def firstUniqChar(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        memory = set()
-        for i, letter in enumerate(s):
-            if letter not in memory:
-                pos = s.find(letter, i+1)
-                if pos == -1:
-                    return i
-                else:
-                    memory.add(letter)
-        return -1
-                
-
-        # this should have been faster since it is O(n)
-#         memory = {}
-#         for letter in s:
-#             if letter in memory.keys():
-#                 memory[letter] += 1
-#             else:
-#                 memory[letter] = 1
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = [0]*26
+        for t in s:
+            i = ord(t) - ord('a')
+            count[i] += 1
             
-#         for i, letter in enumerate(s):
-#             if memory[letter] == 1:
-#                 return i
-        
-#         return -1
+        for j, t in enumerate(s):
+            i = ord(t) - ord('a')
+            if count[i] == 1:
+                return j
+            
+        return -1
+            
