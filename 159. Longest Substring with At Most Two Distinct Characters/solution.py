e90e876b12ba88b6eb7227037a2d3aa8fdfbffa6
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-19 19:56:38 -0400

    Update solution.py

--- 159. Longest Substring with At Most Two Distinct Characters/solution.py
+++ 159. Longest Substring with At Most Two Distinct Characters/solution.py
@@ -1,34 +1,24 @@
-class Solution(object):
-    def lengthOfLongestSubstringTwoDistinct(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        maxLen = 0
-        currLen = 0
-        prev = None
-        prevLen = 0
-        substring = set()
-        for char in s:
-            if char in substring:
-                if prev == char:
-                    prevLen += 1
-                else:
-                    prevLen = 1
-                    prev = char
-                currLen += 1
-            else:
-                if len(substring) == 2:
-                    maxLen = max(maxLen, currLen)
-                    for sub in substring:
-                        if sub != prev:
-                            substring.remove(sub)
-                            break
-
-                currLen = prevLen + 1                    
-                prevLen = 1     
-                substring.add(char)
-                
-            prev = char
-        return max(maxLen, currLen)
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        if len(s) < 3:
+            return len(s)
+        
+        distinct = {}
+        l, r = 0, 0
+        count = 0
+        while r < len(s):
+            if s[r] not in distinct:
+                distinct[s[r]] = 0
+            distinct[s[r]] += 1
+                     
+            r += 1
+            
+            while len(distinct) > 2:
+                distinct[s[l]] -= 1
+                if not distinct[s[l]]:
+                    distinct.pop(s[l])
+                l += 1
                     
+            count = max(count, r - l)
+            
+        return count
