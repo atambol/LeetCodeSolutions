58ed24733b325362b5118de1f053f98df03db09f
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-19 14:16:54 -0400

    Update solution-fast.py

--- 15. 3Sum/solution-fast.py
+++ 15. 3Sum/solution-fast.py
@@ -1,35 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         sol = []
-        
-        # edge case
         if not nums:
             return sol
         
-        # sort the nums
         nums.sort()
-        
-        # loop over each one
         for i in range(len(nums)):
-            j = i + 1
-            k = len(nums) - 1
             if i and nums[i] == nums[i-1]:
                 continue
                 
+            j = i + 1
+            k = len(nums) - 1
             while j < k:
-                target = nums[i] + nums[j] + nums[k]
-                if target == 0:
-                    sol.append([nums[i], nums[j], nums[k]])
+                total = nums[i] + nums[j] + nums[k]
+                if total == 0:
+                    sol.append((nums[i], nums[j], nums[k]))
                     j += 1
                     k -= 1
-                    while nums[j] == nums[j-1] and j < k:
+                                        
+                    while j < k and nums[j] == nums[j-1]:
                         j += 1
-                    while nums[k] == nums[k+1] and j < k:
+
+                    while j < k and k+1 < len(nums) and nums[k] == nums[k+1]:
                         k -= 1
-                elif target > 0:
+                        
+                elif total > 0:
                     k -= 1
                 else:
                     j += 1
-                    
+
         return sol
                     
