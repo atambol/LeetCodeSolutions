1611dcfda177bb81878d1a7659fe451c880a5aa6
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-24 14:35:57 -0400

    Update solution.py

--- 746. Min Cost Climbing Stairs/solution.py
+++ 746. Min Cost Climbing Stairs/solution.py
@@ -1,23 +1,16 @@
 class Solution:
-    def minCostClimbingStairs(self, cost):
-        """
-        :type cost: List[int]
-        :rtype: int
-        """
-        # edge cases
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        total = [0]*len(cost)
         if not cost:
             return 0
+        
         if len(cost) <= 2:
             return min(cost)
+            
+        total[0] = cost[0]
+        total[1] = cost[1]
         
-        # init
-        total = []
-        for c in cost:
-            total.append(c)
-        
-        # DP
         for i in range(2, len(cost)):
-            total[i] += min(total[i-1], total[i-2])
+            total[i] = cost[i] + min(total[i-1], total[i-2])
             
         return min(total[-1], total[-2])
-            
