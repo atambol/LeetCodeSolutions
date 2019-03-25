12d9d461ed8ae2d73c21658e702d53b1e9126a41
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-25 15:43:30 -0400

    Update solution.py

--- 167. Two Sum II - Input array is sorted/solution.py
+++ 167. Two Sum II - Input array is sorted/solution.py
@@ -1,23 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def findTarget(self, root: TreeNode, k: int) -> bool:
-        seen = set()
-        stack = []
-        node = root
-        while node or stack:
-            if node:
-                if node.val in seen:
-                    return True
-                seen.add(k-node.val)
-                stack.append(node.right)
-                node = node.left
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers)-1
+        while i < j and numbers[i] + numbers[j] != target:
+            if numbers[i] + numbers[j] > target:
+                j -= 1
             else:
-                node = stack.pop()
+                i += 1
                 
-        return False
+        return [i+1, j+1]
