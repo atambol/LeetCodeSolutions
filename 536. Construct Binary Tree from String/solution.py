98c163417013b2b8ec1a87f7cdb567ec50ab6f13
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-25 01:40:16 -0400

    Update solution.py

--- 536. Construct Binary Tree from String/solution.py
+++ 536. Construct Binary Tree from String/solution.py
@@ -7,39 +7,31 @@
 
 class Solution:
     def str2tree(self, s: str) -> TreeNode:
-        # edge case
         if not s:
             return None
         
-        # extract nodes
         stack = []
-        left = 1
-        right = 2
-        done = 3
-        num = []
         i = 0
+        num = []
         while i < len(s):
-            if s[i] == "(":
-                pass
-            elif s[i] == ")":
-                node, _ = stack.pop()
-                parent, status = stack.pop()
-                if status == left:
-                    parent.left = node
-                    stack.append((parent, right))
-                else:
+            if s[i] == ")":
+                node = stack.pop()
+                parent = stack.pop()
+                if parent.left:
                     parent.right = node
-                    stack.append((parent, done))
-                    
+                else:
+                    parent.left = node
+                stack.append(parent)
+                i += 1
+            elif s[i] == "(":
+                i += 1
             else:
-                j = i
-                while i + 1 < len(s) and s[i + 1] not in "()":
+                while i < len(s) and s[i] not in "()":
+                    num.append(s[i])
                     i += 1
-                val = s[j:i+1]
-                node = TreeNode(val)
-                stack.append((node, left))
-            i += 1
+                val = int("".join(num))
+                node = TreeNode(val)    
+                stack.append(node)
+                num = []
                 
-        return stack[0][0]
-                
-                
+        return stack.pop()
