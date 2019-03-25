ed92c64b767a77f11650f930f1ff885e4233942d
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-03-25 01:07:51 -0400

    Update solution.py

--- 424. Longest Repeating Character Replacement/solution.py
+++ 424. Longest Repeating Character Replacement/solution.py
@@ -1,24 +1,27 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        counts = [0]*26
-        i = 0 
-        j = 0
-        maxLen = 0
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if not s:
+            return 0
         
-        # Sliding window
+        count = [0]*26
+        maxlen = 0
+        i = 0
+        j = 0
         while j < len(s):
-            # insert letter count
-            index = ord(s[j]) - ord("A")
-            counts[index] += 1
+            ind = ord(s[j]) - ord('A')
+            count[ind] += 1
             j += 1
-
-            # update counts array so as to have max k updates
-            while sum(counts) - max(counts) > k:
-                index = ord(s[i]) - ord("A")
-                counts[index] -= 1
+            while sum(count) - max(count) > k:
+                ind = ord(s[i]) - ord('A')
+                count[ind] -= 1
                 i += 1
-    
-            maxLen = max(maxLen, sum(counts))
+                
+            maxlen = max(maxlen, j-i)
+
             
-            
-        return maxLen
+        return maxlen
