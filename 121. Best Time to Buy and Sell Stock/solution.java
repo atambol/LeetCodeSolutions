ea47452f9e9b26d56d0ea90c7a156a6f0d27cce9
carbonite <anirudhatambolkar@gmail.com>
2019-09-26 23:16:38 -0700

    Create solution.java

---
+++ 121. Best Time to Buy and Sell Stock/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int profit = 0;
+        int minStock = prices[0];
+        
+        for (int i = 1; i < prices.length; i++) {
+            minStock = Math.min(minStock, prices[i]);
+            profit = Math.max(profit, prices[i] - minStock);
+        }
+        
+        return profit;
+    }
+}
