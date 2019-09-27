1983b2abc1f30fbe980bd3086c6d59962851367b
carbonite <anirudhatambolkar@gmail.com>
2019-09-26 22:54:17 -0700

    Create solution.java

---
+++ 53. Maximum Subarray/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        
+        for (int num: nums) {
+            sum = Math.max(sum + num, num);
+            maxSum = Math.max(sum, maxSum);
+        }
+        
+        return maxSum;
+    }
+}
