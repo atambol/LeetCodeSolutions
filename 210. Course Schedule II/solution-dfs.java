4c0ea883b37febbf8d43e1036fdeda6df438f1a9
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-12-23 00:36:04 -0800

    Update solution-dfs.java

--- 210. Course Schedule II/solution-dfs.java
+++ 210. Course Schedule II/solution-dfs.java
@@ -1,65 +1,59 @@
 class Solution {
-    enum State {
-        visited, visiting, unvisited;
-    }
+    private int visited = 2;
+    private int visiting = 1;
+    private int unvisited = 0;
     
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // State array stores visited information
-        State[] states = new State[numCourses];
+        Map<Integer, List<Integer>> graph = getGraph(numCourses, prerequisites);
+        int[] visitStatus = new int[numCourses];
+        List<Integer> topSort = new ArrayList<Integer>();
+        int[] topSortArray = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            states[i] = State.unvisited;
-        }
-        
-        // create adjacency list
-        List<Integer>[] adj = new List[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-        
-        for (int i = 0; i < prerequisites.length; i++) {
-            adj[prerequisites[i][0]].add(prerequisites[i][1]);
-        }
-        
-        // solution
-        int[] order = new int[numCourses];
-        
-        // dfs on all
-        int ptr = 0;
-        for (int course = 0; course < numCourses; course++) {
-            if (states[course] == State.visited) {
-                continue;
-            }
-            ptr = dfs(numCourses, course, adj, order, ptr, states);
-            
-            // cycle found, cannot take courses
-            if (ptr == -1) {
+            if (dfs(i, graph, visitStatus, topSort) == false) {
                 return new int[0];
             }
         }
-        return order;
+        
+        int i = 0;
+        for (int course: topSort) {
+            topSortArray[i++] = course;
+        }
+        return topSortArray;
     }
     
-    public int dfs(int numCourses, int course, List<Integer>[] adj, int[] order, int ptr, State[] states) {
-        if (states[course] == State.visiting) {
-            return -1;
+    public boolean dfs(int course, 
+                    Map<Integer, List<Integer>> graph, 
+                    int[] visitStatus,
+                    List<Integer> topSort) {
+        if (visitStatus[course] == visiting) {
+            return false;
+        }
+        if (visitStatus[course] == visited) {
+            return true;
+        }
+        visitStatus[course] = visiting;
+        List<Integer> adjList = graph.get(course);
+        for (Integer prereq: adjList) {
+            if (dfs(prereq, graph, visitStatus, topSort) == false) {
+                return false;
+            }
+        }
+        visitStatus[course] = visited;
+        topSort.add(course);
+        return true;
+    }
+    
+    public Map<Integer, List<Integer>> getGraph(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.put(i, new ArrayList<Integer>());
         }
         
-        states[course] = State.visiting;
-        for (int prereq: adj[course]) {
-            if (states[prereq] == State.visited) {
-                continue;
-            }
-            else {
-                ptr = dfs(numCourses, prereq, adj, order, ptr, states);
-                if (ptr == -1) {
-                    return -1;
-                }
-            }
+        List<Integer> adjList;
+        for (int[] edge: prerequisites) {
+            adjList = graph.get(edge[0]);
+            adjList.add(edge[1]);
         }
-        
-        order[ptr] = course;
-        states[course] = State.visited;
-        ptr++;
-        return ptr; 
-    } 
+        return graph;
+    }
 }
