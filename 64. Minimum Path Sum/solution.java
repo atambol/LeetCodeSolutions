ec3dcfd460d81e970e322b47ddb07e9d325f6d02
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2019-12-23 19:55:31 -0800

    Update solution.java

--- 64. Minimum Path Sum/solution.java
+++ 64. Minimum Path Sum/solution.java
@@ -1,37 +1,36 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        if (m == 0) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        if (grid[0].length == 0) {
             return 0;
         }
         
-        int n = grid[0].length;
-        if (n == 0) {
-            return 0;
-        }
-        
-        int[][] dp = new int[m][n];
-        
-        // initialise 0,0 
-        dp[0][0] = grid[0][0];
-        
-        // initialise the first row
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = dp[0][i-1] + grid[0][i];
-        }
-        
-        // initialise the first column
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
-        
-        // dp all the way
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                dp[i][j] = 0;
             }
         }
         
-        return dp[m-1][n-1];
+        int minPath = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    minPath = 0;
+                } else if (i == 0 && j != 0) {
+                    minPath = dp[i][j-1];
+                } else if (i != 0 && j == 0) {
+                    minPath = dp[i-1][j];
+                } else {
+                    minPath = Math.min(dp[i-1][j], dp[i][j-1]);
+                }
+                
+                dp[i][j] = minPath + grid[i][j];
+            }
+        }
+        
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
