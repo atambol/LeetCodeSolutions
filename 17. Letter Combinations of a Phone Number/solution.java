54d8a6300ba562217f7af27d23436e7be13db388
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2020-01-03 15:57:43 -0800

    Update solution.java

--- 17. Letter Combinations of a Phone Number/solution.java
+++ 17. Letter Combinations of a Phone Number/solution.java
@@ -1,45 +1,32 @@
 class Solution {
-    private static final Map<String, String[]> keyMap = Collections.unmodifiableMap(
-    new HashMap<String, String[]>() {{
-        put("2", new String[] {"a", "b", "c"});
-        put("3", new String[] {"d", "e", "f"});
-        put("4", new String[] {"g", "h", "i"});
-        put("5", new String[] {"j", "k", "l"});
-        put("6", new String[] {"m", "n", "o"});
-        put("7", new String[] {"p", "q", "r", "s"});
-        put("8", new String[] {"t", "u", "v"});
-        put("9", new String[] {"w", "x", "y", "z"});
-    }});
+    private Map<String, List<String>> map;
     
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
             return new ArrayList<String>();
         }
-        return backtrack(0, digits);
+        map = new HashMap<>();
+        map.put("2", Arrays.asList("a", "b", "c"));
+        map.put("3", Arrays.asList("d", "e", "f"));
+        map.put("4", Arrays.asList("g", "h", "i"));
+        map.put("5", Arrays.asList("j", "k", "l"));
+        map.put("6", Arrays.asList("m", "n", "o"));
+        map.put("7", Arrays.asList("p", "q", "r", "s"));
+        map.put("8", Arrays.asList("t", "u", "v"));
+        map.put("9", Arrays.asList("w", "x", "y", "z"));
+        
+        return backtrack(digits);
     }
     
-    public List<String> backtrack(int pos, String digits) {
+    public List<String> backtrack(String digits) {
+        if (digits.length() == 1)
+            return map.get(digits.substring(0, 1));
         List<String> sol = new ArrayList<String>();
-        char c = digits.charAt(pos);
-        String s = String.valueOf(c);
-
-        // base case
-        if (pos == digits.length()-1) {
-            for (String key: keyMap.get(s)) {
-                String tmp = new String();
-                tmp = key;
-                sol.add(tmp);
-            }
-            return sol;
-        }
-        
-        // intermediate case
-        List<String> sol_intermediate = backtrack(pos+1, digits);
-        for (String key: keyMap.get(s)) {
-            for (String suffix: sol_intermediate) {
-                 sol.add(key+suffix);
-            }
-        }
+        List<String> subsol = backtrack(digits.substring(1, digits.length()));
+        String cur = digits.substring(0, 1);
+        for (String s: subsol)
+            for (String c: map.get(cur))
+                sol.add(c + s);
         return sol;
     }
 }
