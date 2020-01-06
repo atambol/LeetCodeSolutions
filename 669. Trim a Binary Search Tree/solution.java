a76fef7526b78185a09dcfacd01710b5ede8604e
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2020-01-05 23:49:58 -0800

    Update solution.java

--- 669. Trim a Binary Search Tree/solution.java
+++ 669. Trim a Binary Search Tree/solution.java
@@ -11,16 +11,16 @@ class Solution {
     public TreeNode trimBST(TreeNode root, int L, int R) {
         if (root == null) {
             return root;
+        }
+        
+        if (root.val > R) {
+            return trimBST(root.left, L, R);
+        } else if (root.val < L) {
+            return trimBST(root.right, L, R);
         } else {
-            if (root.val < L) {
-                return trimBST(root.right, L, R);
-            } else if (root.val > R) {
-                return trimBST(root.left, L, R);
-            } else {
-                root.left = trimBST(root.left, L, R);
-                root.right = trimBST(root.right, L, R);
-                return root;
-            }
+            root.right = trimBST(root.right, root.val, R);
+            root.left = trimBST(root.left, L, root.val);
+            return root;
         }
     }
 }
