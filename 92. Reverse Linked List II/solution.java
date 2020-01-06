7a1865ec5d17020ee8e75c5e0929624aefc4a862
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2020-01-06 00:03:18 -0800

    Update solution.java

--- 92. Reverse Linked List II/solution.java
+++ 92. Reverse Linked List II/solution.java
@@ -8,37 +8,28 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        // edge cases
-        if (head == null || head.next == null) {
-            return head;
-        } 
-        
-        if (m == n) {
-            return head;
-        }
-        
-        // add a dummy head
+        // insert dummy node
         ListNode node = new ListNode(0);
         node.next = head;
         head = node;
-        ListNode tail1, tmp, tail2, prev;
-        prev = node;
-        node = node.next;
         
-        // reach mth node
-        while (m != 1) {
+        // arrive at the mth node
+        ListNode prev = head;
+        node = head.next;
+        
+        while (m > 1) {
             m--;
             n--;
             prev = node;
             node = node.next;
         }
         
-        tail1 = prev;
-        tail2 = node;
-        
-        // reverse until nth node
+        // reverse m-n nodes
+        ListNode tail = prev;
+        ListNode head2 = node;
+        ListNode tmp;
         prev = null;
-        while (n != 0) {
+        while (n > 0) {
             n--;
             tmp = node.next;
             node.next = prev;
@@ -46,10 +37,9 @@ class Solution {
             node = tmp;
         }
         
-        // attach the three parts
-        tail2.next = node;
-        tail1.next = prev;
-        
+        // reattach and return
+        tail.next = prev;
+        head2.next = node;
         return head.next;
     }
 }
